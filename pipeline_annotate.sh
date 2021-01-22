d2a581e82af7e566dc1cf5a12af2ba802f559a5e
mkslofstra <mariska-karina@hotmail.com>
2021-01-22 13:06:13 +0100

    style: undo reformatting

--- pipeline_annotate.sh
+++ pipeline_annotate.sh
@@ -24,7 +24,8 @@ CPU_CORES=4
 FORCE=0
 KEEP=0
 
-usage() {
+usage()
+{
   echo "usage: pipeline_annotate.sh -i <arg> -o <arg> [-r <arg>] [-c <arg>] [-a <arg>] [-f] [-k]
 
 -i,  --input  <arg>        required: Input VCF file (.vcf or .vcf.gz).
@@ -48,92 +49,101 @@ examples:
 PARSED_ARGUMENTS=$(getopt -a -n pipeline -o i:o:r:a:c:fk --long input:,output:,reference:,assembly:,cpu_cores:,force,keep,ann_vep: -- "$@")
 VALID_ARGUMENTS=$?
 if [ "$VALID_ARGUMENTS" != "0" ]; then
-  usage
-  exit 2
+	usage
+	exit 2
 fi
 
 eval set -- "$PARSED_ARGUMENTS"
-while :; do
+while :
+do
   case "$1" in
-  -i | --input)
-    INPUT=$(realpath "$2")
-    shift 2
-    ;;
-  -o | --output)
-    OUTPUT_ARG="$2"
-    OUTPUT_DIR_RELATIVE=$(dirname "$OUTPUT_ARG")
-    OUTPUT_DIR_ABSOLUTE=$(realpath "$OUTPUT_DIR_RELATIVE")
-    OUTPUT_FILE=$(basename "$OUTPUT_ARG")
-    OUTPUT="${OUTPUT_DIR_ABSOLUTE}"/"${OUTPUT_FILE}"
-    shift 2
-    ;;
-  -r | --reference)
-    INPUT_REF=$(realpath "$2")
-    shift 2
-    ;;
-  -c | --cpu_cores)
-    CPU_CORES="$2"
-    shift 2
-    ;;
-  -a | --assembly)
-    ASSEMBLY="$2"
-    shift 2
-    ;;
-  --ann_vep)
-    ANN_VEP="$2"
-    shift 2
-    ;;
-  -f | --force)
-    FORCE=1
-    shift
-    ;;
-  -k | --keep)
-    KEEP=1
-    shift
-    ;;
-  --)
-    shift
-    break
-    ;;
-  *)
-    usage
-    exit 2
-    ;;
+    -i | --input)
+      INPUT=$(realpath "$2")
+      shift 2
+      ;;
+    -o | --output)
+      OUTPUT_ARG="$2"
+      OUTPUT_DIR_RELATIVE=$(dirname "$OUTPUT_ARG")
+      OUTPUT_DIR_ABSOLUTE=$(realpath "$OUTPUT_DIR_RELATIVE")
+      OUTPUT_FILE=$(basename "$OUTPUT_ARG")
+      OUTPUT="${OUTPUT_DIR_ABSOLUTE}"/"${OUTPUT_FILE}"
+      shift 2
+      ;;
+    -r | --reference)
+      INPUT_REF=$(realpath "$2")
+      shift 2
+      ;;
+    -c | --cpu_cores)
+      CPU_CORES="$2"
+      shift 2
+      ;;
+    -a | --assembly)
+      ASSEMBLY="$2"
+      shift 2
+      ;;
+    --ann_vep)
+      ANN_VEP="$2"
+      shift 2
+      ;;
+    -f | --force)
+      FORCE=1
+      shift
+      ;;
+    -k | --keep)
+      KEEP=1
+      shift
+      ;;
+    --)
+      shift
+      break
+      ;;
+    *)
+      usage
+	    exit 2
+      ;;
   esac
 done
 
-if [ -z "${INPUT}" ]; then
+if [ -z "${INPUT}" ]
+then
   echo -e "missing required option -i\n"
-  usage
-  exit 2
+	usage
+	exit 2
 fi
-if [ -z "${OUTPUT}" ]; then
+if [ -z "${OUTPUT}" ]
+then
   echo -e "missing required option -o\n"
-  usage
-  exit 2
+	usage
+	exit 2
 fi
 
-if [ ! -f "${INPUT}" ]; then
-  echo -e "$INPUT does not exist.\n"
-  exit 2
+if [ ! -f "${INPUT}" ]
+then
+	echo -e "$INPUT does not exist.\n"
+	exit 2
 fi
-if [ -f "${OUTPUT}" ]; then
-  if [ "${FORCE}" == "1" ]; then
-    rm "${OUTPUT}"
-  else
-    echo -e "${OUTPUT} already exists, use -f to overwrite.\n"
+if [ -f "${OUTPUT}" ]
+then
+	if [ "${FORCE}" == "1" ]
+	then
+		rm "${OUTPUT}"
+	else
+		echo -e "${OUTPUT} already exists, use -f to overwrite.\n"
     exit 2
-  fi
+	fi
 fi
-if [ ! -z "${INPUT_REF}" ]; then
-  if [ ! -f "${INPUT_REF}" ]; then
+if [ ! -z "${INPUT_REF}" ]
+then
+  if [ ! -f "${INPUT_REF}" ]
+  then
     echo -e "${INPUT_REF} does not exist.\n"
     exit 2
   fi
 fi
 
+
 if [ -z "${TMPDIR+x}" ]; then
-  TMPDIR=/tmp
+	TMPDIR=/tmp
 fi
 
 #VcfAnno
@@ -145,7 +155,7 @@ VCFANNO_PRE_CONF="${VCFANNO_OUTPUT_DIR}"/conf_pre.toml
 rm -rf "${VCFANNO_OUTPUT_DIR}"
 mkdir -p "${VCFANNO_OUTPUT_DIR}"
 
-cat >"${VCFANNO_PRE_CONF}" <<EOT
+cat > "${VCFANNO_PRE_CONF}" << EOT
 [[annotation]]
 file="/apps/data/VKGL/vkgl_consensus_jun2020_normalized.vcf.gz"
 fields = ["VKGL_CL", "AMC", "EMC", "LUMC", "NKI", "RMMC", "UMCG", "UMCU", "VUMC"]
@@ -175,7 +185,7 @@ module load "${MOD_VCF_ANNO}"
 module load "${MOD_HTS_LIB}"
 
 VCFANNO_ARGS=("-p" "${CPU_CORES}" "${VCFANNO_PRE_CONF}" "${VCFANNO_INPUT}")
-vcfanno "${VCFANNO_ARGS[@]}" | bgzip >"${VCFANNO_OUTPUT}"
+vcfanno "${VCFANNO_ARGS[@]}" | bgzip > "${VCFANNO_OUTPUT}"
 
 module purge
 
@@ -186,7 +196,8 @@ BCFTOOLS_FILTER_OUTPUT="${CAPICE_OUTPUT_DIR}"/vcfanno_bcftools_filter.vcf.gz
 CAPICE_INPUT="${BCFTOOLS_FILTER_OUTPUT}"
 VCFANNO_POST_CONF="${CAPICE_OUTPUT_DIR}"/conf_post.toml
 
-if [[ "${OUTPUT_FILE}" == *vcf ]]; then
+if [[ "${OUTPUT_FILE}" == *vcf ]]
+then
   CAPICE_OUTPUT="${CAPICE_OUTPUT_DIR}"/"${OUTPUT_FILE/.vcf/.tsv}"
 else
   CAPICE_OUTPUT="${CAPICE_OUTPUT_DIR}"/"${OUTPUT_FILE/.vcf.gz/.tsv}"
@@ -197,38 +208,40 @@ rm -rf "${CAPICE_OUTPUT_DIR}"
 mkdir -p "${CAPICE_OUTPUT_DIR}"
 
 module load "${MOD_BCF_TOOLS}"
-bcftools filter -i 'CAP="."' --threads "${CPU_CORES}" "${BCFTOOLS_FILTER_INPUT}" | bgzip -c >"${BCFTOOLS_FILTER_OUTPUT}"
+bcftools filter -i 'CAP="."' --threads "${CPU_CORES}" "${BCFTOOLS_FILTER_INPUT}" | bgzip -c > "${BCFTOOLS_FILTER_OUTPUT}"
 module purge
 
-if [ $(zgrep -c -m 1 "^[^#]" "${BCFTOOLS_FILTER_OUTPUT}") -eq 0 ]; then
-  VCFANNO_ALL_OUTPUT="${BCFTOOLS_FILTER_INPUT}"
-  echo "skipping CAPICE score calculation because all variants have precomputed scores ..."
+if [ `zgrep -c -m 1 "^[^#]" "${BCFTOOLS_FILTER_OUTPUT}"` -eq 0 ]
+then
+	VCFANNO_ALL_OUTPUT="${BCFTOOLS_FILTER_INPUT}"
+	echo "skipping CAPICE score calculation because all variants have precomputed scores ..."
 else
-  VCFANNO_ALL_OUTPUT="${CAPICE_OUTPUT_DIR}"/vcfanno_all.vcf.gz
-  echo "calculating CAPICE scores for variants without precomputed score ..."
+	VCFANNO_ALL_OUTPUT="${CAPICE_OUTPUT_DIR}"/vcfanno_all.vcf.gz
+	echo "calculating CAPICE scores for variants without precomputed score ..."
 
-  module load "${MOD_CADD}"
-  # strip headers from input vcf for cadd
-  CADD_INPUT="${CAPICE_OUTPUT_DIR}/input_headerless_$(date +%s).vcf.gz"
-  gunzip -c "$CAPICE_INPUT" | sed '/^#/d' | bgzip >"${CADD_INPUT}"
-  CADD_ARGS=("-a" "-g" "${ASSEMBLY}" "-o" "${CAPICE_OUTPUT_DIR}/cadd.tsv.gz" "-c" "${CPU_CORES}" "-s" "${CADD_INPUT}")
-  CADD.sh "${CADD_ARGS[@]}"
-  module purge
+	module load "${MOD_CADD}"
+	# strip headers from input vcf for cadd
+	CADD_INPUT="${CAPICE_OUTPUT_DIR}/input_headerless_$(date +%s).vcf.gz"
+	gunzip -c "$CAPICE_INPUT" | sed '/^#/d' | bgzip > "${CADD_INPUT}"
+	CADD_ARGS=("-a" "-g" "${ASSEMBLY}" "-o" "${CAPICE_OUTPUT_DIR}/cadd.tsv.gz" "-c" "${CPU_CORES}" "-s" "${CADD_INPUT}")
+	CADD.sh "${CADD_ARGS[@]}"
+	module purge
 
-  module load "${MOD_CAPICE}"
-  PYTHON_ARGS=("${EBROOTCAPICE}/CAPICE_scripts/model_inference.py" "--input_path" "${CAPICE_OUTPUT_DIR}/cadd.tsv.gz" "--model_path" "${EBROOTCAPICE}/CAPICE_model/${ASSEMBLY}/xgb_booster.pickle.dat" "--prediction_savepath" "${CAPICE_OUTPUT}")
-  python "${PYTHON_ARGS[@]}"
+	module load "${MOD_CAPICE}"
+	PYTHON_ARGS=("${EBROOTCAPICE}/CAPICE_scripts/model_inference.py" "--input_path" "${CAPICE_OUTPUT_DIR}/cadd.tsv.gz" "--model_path" "${EBROOTCAPICE}/CAPICE_model/${ASSEMBLY}/xgb_booster.pickle.dat" "--prediction_savepath" "${CAPICE_OUTPUT}")
+	python "${PYTHON_ARGS[@]}"
 
-  CAPICE_ARGS=("-Djava.io.tmpdir=${TMPDIR}" "-XX:ParallelGCThreads=2" "-Xmx1g" "-jar" "${EBROOTCAPICE}/capice2vcf.jar" "-i" "${CAPICE_OUTPUT}" "-o" "${CAPICE_OUTPUT_VCF}")
+	CAPICE_ARGS=("-Djava.io.tmpdir=${TMPDIR}" "-XX:ParallelGCThreads=2" "-Xmx1g" "-jar" "${EBROOTCAPICE}/capice2vcf.jar" "-i" "${CAPICE_OUTPUT}" "-o" "${CAPICE_OUTPUT_VCF}")
 
-  if [ "${FORCE}" == "1" ]; then
-    CAPICE_ARGS+=("-f")
-  fi
-  java "${CAPICE_ARGS[@]}"
+	if [ "${FORCE}" == "1" ]
+	then
+		CAPICE_ARGS+=("-f")
+	fi
+	java "${CAPICE_ARGS[@]}"
 
-  module purge
+	module purge
 
-  cat >"${VCFANNO_POST_CONF}" <<EOT
+	cat > "${VCFANNO_POST_CONF}" << EOT
 [[annotation]]
 file="${CAPICE_OUTPUT_VCF}"
 fields = ["CAP"]
@@ -236,14 +249,16 @@ ops=["self"]
 names=["CAP"]
 EOT
 
-  module load "${MOD_VCF_ANNO}"
-  module load "${MOD_HTS_LIB}"
+	module load "${MOD_VCF_ANNO}"
+	module load "${MOD_HTS_LIB}"
 
-  VCFANNO_ARGS=("-p" "${CPU_CORES}" "${VCFANNO_POST_CONF}" "${VCFANNO_OUTPUT}")
-  vcfanno "${VCFANNO_ARGS[@]}" | bgzip >"${VCFANNO_ALL_OUTPUT}"
+	VCFANNO_ARGS=("-p" "${CPU_CORES}" "${VCFANNO_POST_CONF}" "${VCFANNO_OUTPUT}")
+	vcfanno "${VCFANNO_ARGS[@]}" | bgzip > "${VCFANNO_ALL_OUTPUT}"
 
-  module purge
+	module purge
 fi
+
+
 
 # VEP
 VEP_INPUT="${VCFANNO_ALL_OUTPUT}"
@@ -256,8 +271,9 @@ mkdir -p "${VEP_OUTPUT_DIR}"
 module load "${MOD_VEP}"
 VEP_ARGS=("--input_file" "${VEP_INPUT}" "--format" "vcf")
 VEP_ARGS+=("--output_file" "${VEP_OUTPUT}" "--vcf")
-if [[ "${OUTPUT}" == *.vcf.gz ]]; then
-  VEP_ARGS+=("--compress_output" "bgzip")
+if [[ "${OUTPUT}" == *.vcf.gz ]]
+then
+	VEP_ARGS+=("--compress_output" "bgzip")
 fi
 
 VEP_ARGS+=("--stats_file" "${VEP_OUTPUT_STATS}" "--stats_text")
@@ -276,12 +292,12 @@ VEP_ARGS+=("--allow_non_variant")
 VEP_ARGS+=("--fork" "${CPU_CORES}")
 
 if [ -n "${INPUT_REF}" ]; then
-  VEP_ARGS+=("--fasta" "${INPUT_REF}" "--hgvs")
+	VEP_ARGS+=("--fasta" "${INPUT_REF}" "--hgvs")
 fi
 
 if [ -n "${ANN_VEP}" ]; then
-  # shellcheck disable=SC2206
-  VEP_ARGS+=(${ANN_VEP})
+	# shellcheck disable=SC2206
+	VEP_ARGS+=(${ANN_VEP})
 fi
 
 vep "${VEP_ARGS[@]}"
--- test/pipeline_test.sh
+++ test/pipeline_test.sh
@@ -12,9 +12,10 @@ LOG='./test/output/test_output.log'
 
 echo -e "Test started..."
 
-bash ./pipeline.sh -i "${INPUT}" -o "${ACTUAL_VCF}.gz" -p "${PED}" -t "${HPO}" -f &>"${LOG}"
+bash ./pipeline.sh -i "${INPUT}" -o "${ACTUAL_VCF}.gz" -p "${PED}" -t "${HPO}" -f &> "${LOG}"
 
-if [ $? -eq 0 ]; then
+if [ $? -eq 0 ]
+then
   echo -e "\e[32mPipeline ran succesfully.  \e[39m"
 else
   echo -e "\e[31mAn error occured while running the pipeline, see './test/output/test_output.log' for more details.  \e[39m"
@@ -22,17 +23,19 @@ else
   exit 1
 fi
 
-if [ -f ${ACTUAL_VCF} ]; then
+if [ -f ${ACTUAL_VCF} ]
+then
   rm ${ACTUAL_VCF}
 fi
 gunzip ${ACTUAL_VCF}.gz
 
 HEADERS_COUNT=$(grep '^##' ${ACTUAL_VCF} | wc -l)
-if [ ${EXPECTED_NR_OF_HEADERS} != ${HEADERS_COUNT} ]; then
+if [ ${EXPECTED_NR_OF_HEADERS} != ${HEADERS_COUNT} ]
+then
   echo -e "\e[31mNr of vcf headers test failed: expected: ${EXPECTED_NR_OF_HEADERS} got: ${HEADERS_COUNT} \e[39m"
   FAILED=1
 else
-  echo -e "\e[32mNr of vcf headers test passed. \e[39m"
+    echo -e "\e[32mNr of vcf headers test passed. \e[39m"
 fi
 
 #remove parts that differ per run or environment before comparing output.
@@ -44,30 +47,33 @@ sed -i '/^##VIP/d' ${ACTUAL_VCF}
 sed -i 's/"CAPICE pathogenicity prediction.*/"CAPICE pathogenicity prediction"/g' ${ACTUAL_VCF}
 
 VCF_DIFF=$(diff $ACTUAL_VCF $EXPECTED_VCF)
-if [ "$VCF_DIFF" != "" ]; then
+if [ "$VCF_DIFF" != "" ]
+then
   echo -e "\e[31mvcf file test failed, output file differs from expected: \e[39m"
   echo "---BEGIN diff---"
-  echo $VCF_DIFF
-  echo "---END diff---"
-  FAILED=1
+	echo $VCF_DIFF
+	echo "---END diff---"
+	FAILED=1
 else
-  echo -e "\e[32mvcf file test passed. \e[39m"
+    echo -e "\e[32mvcf file test passed. \e[39m"
 fi
 
 sed -i 's/"args":"-i.*step2_capice/REMOVE_PATH_DEPENDENT_VALUES/g' test/output/test_output.html
 
 REPORT_DIFF=$(diff $ACTUAL_HTML $EXPECTED_HTML)
-if [ "$REPORT_DIFF" != "" ]; then
-  echo -e "\e[31report file test failed, output file differs from expected, \e[39m"
+if [ "$REPORT_DIFF" != "" ]
+then
+    	echo -e "\e[31report file test failed, output file differs from expected, \e[39m"
   echo "run 'diff $ACTUAL_HTML $EXPECTED_HTML}' for more information."
   FAILED=1
 else
-  echo -e "\e[32mreport file test passed. \e[39m"
+    echo -e "\e[32mreport file test passed. \e[39m"
 fi
 
 echo -e "Test finished..."
 
-if [ "${FAILED}" == 1 ]; then
+if [ "${FAILED}" == 1 ]
+then
   exit 1
 else
   exit 0
