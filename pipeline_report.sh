ac87c39d0927978e536c3a133182409134cfeaa7
Dennis Hendriksen <d.hendriksen@umcg.nl>
2021-03-04 14:48:40 +0100

    fix

--- pipeline_report.sh
+++ pipeline_report.sh
@@ -42,8 +42,8 @@ config:
 # arguments:
 #   $1 path to input file
 #   $2 path to output file
-#   $3 path to pedigree file (optional)
-#   $4 probands (optional)
+#   $3 probands (optional)
+#   $4 path to pedigree file (optional)
 #   $5 phenotypes (optional)
 #   $6 maxRecords (optional)
 #   $7 maxSamples (optional)
@@ -51,8 +51,8 @@ config:
 report() {
   local -r inputFilePath="${1}"
   local -r outputFilePath="${2}"
-  local -r pedFilePath="${3}"
-  local -r probands="${4}"
+  local -r probands="${3}"
+  local -r pedFilePath="${4}"
   local -r phenotypes="${5}"
   local -r maxRecords="${6}"
   local -r maxSamples="${7}"
