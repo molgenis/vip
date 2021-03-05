d2d48942a540635a6b30fd23a3e61f64ec703afb
Dennis Hendriksen <d.hendriksen@umcg.nl>
2021-03-05 07:13:24 +0100

    simplify report test

--- test/pipeline_test.sh
+++ test/pipeline_test.sh
@@ -13,10 +13,9 @@ LOG='./test/output/test_output.log'
 
 echo -e "Test started..."
 
-bash ./pipeline.sh -i "${INPUT}" -o "${ACTUAL_VCF}.gz" -p "${PED}" -t "${HPO}" -f &> "${LOG}"
+bash ./pipeline.sh -i "${INPUT}" -o "${ACTUAL_VCF}.gz" -p "${PED}" -t "${HPO}" -f &>"${LOG}"
 
-if [ $? -eq 0 ]
-then
+if [ $? -eq 0 ]; then
   echo -e "\e[32mPipeline ran succesfully.  \e[39m"
 else
   echo -e "\e[31mAn error occured while running the pipeline, see './test/output/test_output.log' for more details.  \e[39m"
@@ -24,19 +23,17 @@ else
   exit 1
 fi
 
-if [ -f ${ACTUAL_VCF} ]
-then
+if [ -f ${ACTUAL_VCF} ]; then
   rm ${ACTUAL_VCF}
 fi
 gunzip ${ACTUAL_VCF}.gz
 
 HEADERS_COUNT=$(grep '^##' ${ACTUAL_VCF} | wc -l)
-if [ ${EXPECTED_NR_OF_HEADERS} != ${HEADERS_COUNT} ]
-then
+if [ ${EXPECTED_NR_OF_HEADERS} != ${HEADERS_COUNT} ]; then
   echo -e "\e[31mNr of vcf headers test failed: expected: ${EXPECTED_NR_OF_HEADERS} got: ${HEADERS_COUNT} \e[39m"
   FAILED=1
 else
-    echo -e "\e[32mNr of vcf headers test passed. \e[39m"
+  echo -e "\e[32mNr of vcf headers test passed. \e[39m"
 fi
 
 #remove parts that differ per run or environment before comparing output.
@@ -48,33 +45,26 @@ sed -i '/^##VIP/d' ${ACTUAL_VCF}
 sed -i 's/"CAPICE pathogenicity prediction.*/"CAPICE pathogenicity prediction"/g' ${ACTUAL_VCF}
 
 VCF_DIFF=$(diff $ACTUAL_VCF $EXPECTED_VCF)
-if [ "$VCF_DIFF" != "" ]
-then
+if [ "$VCF_DIFF" != "" ]; then
   echo -e "\e[31mvcf file test failed, output file differs from expected: \e[39m"
   echo -e "---BEGIN diff---"
-	echo $VCF_DIFF
-	echo -e "---END diff---"
-	FAILED=1
-else
-    echo -e "\e[32mvcf file test passed. \e[39m"
-fi
-
-sed -i 's/"args":"-i.*step2_capice/REMOVE_PATH_DEPENDENT_VALUES/g' test/output/test_output.vcf.gz.html
-
-REPORT_DIFF=$(diff $ACTUAL_HTML $EXPECTED_HTML)
-if [ "$REPORT_DIFF" != "" ]
-then
-    	echo -e "\e[31mreport file test failed, output file differs from expected, \e[39m"
-  echo -e "run 'diff $ACTUAL_HTML $EXPECTED_HTML}' for more information."
+  echo $VCF_DIFF
+  echo -e "---END diff---"
   FAILED=1
 else
-    echo -e "\e[32mreport file test passed. \e[39m"
+  echo -e "\e[32mvcf file test passed. \e[39m"
+fi
+
+if [[ ! -f "${ACTUAL_HTML}" ]]; then
+  echo -e "\e[31mreport file test failed, output file does not exist, \e[39m"
+  FAILED=1
+else
+  echo -e "\e[32mreport file test passed. \e[39m"
 fi
 
 echo -e "Test finished..."
 
-if [ "${FAILED}" == 1 ]
-then
+if [ "${FAILED}" == 1 ]; then
   exit 1
 else
   exit 0
