9c70c28b71cd0df9bb9166f7d098f0d9a9660095
bartcharbon <bart.charbon@gmail.com>
2021-04-15 07:22:06 +0200

    Bump VIBE and VIP version

--- utils/header.sh
+++ utils/header.sh
@@ -17,7 +17,7 @@ fi
 
 declare -A VIP_CFG_MAP
 
-VIP_VERSION="2.0.1"
+VIP_VERSION="2.0.2"
 
 MOD_BCF_TOOLS="BCFtools/1.11-GCCcore-7.3.0"
 MOD_CADD="CADD/v1.4-foss-2018b-minimal"
@@ -28,7 +28,7 @@ MOD_VCF_DECISION_TREE="vcf-decision-tree/v0.0.3-Java-11-LTS"
 MOD_VCF_INHERITANCE_MATCHER="vcf-inheritance-matcher/v0.1.0-Java-11-LTS"
 MOD_VCF_REPORT="vcf-report/v1.9.0-Java-11-LTS"
 MOD_VEP="VEP/103.1-foss-2018b-Perl-5.28.0"
-MOD_VIBE="VIBE/5.0.1-Java-11-LTS"
+MOD_VIBE="VIBE/5.0.3-Java-11-LTS"
 MOD_PYTHON_PLUS="PythonPlus/3.7.4-foss-2018b-v20.11.1"
 MOD_ANNOTSV="AnnotSV/v2.5-GCCcore-7.3.0"
 
