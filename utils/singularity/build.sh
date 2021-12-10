2d2b46d4733f62281c543e35f4dcbd92358b6731
bartcharbon <bart.charbon@gmail.com>
2021-12-10 08:18:12 +0100

    Decision tree version bump + build.sh fixes

--- utils/singularity/build.sh
+++ utils/singularity/build.sh
@@ -77,10 +77,11 @@ main() {
   mkdir -p "${outputDir}/build"
 
   images=()
-  images+=("build/alpine-3.14.2")
+  images+=("build/alpine-3.14.3")
   images+=("build/openjdk-11")
+  images+=("build/htslib-1.14")
   images+=("bcftools-1.14")
-  images+=("gatk-4.2.2.0" "vcf-decision-tree-1.0.0" "vcf-inheritance-matcher-1.0.0" "vcf-report-2.5.2")
+  images+=("gatk-4.2.2.0" "vcf-decision-tree-1.0.1" "vcf-inheritance-matcher-1.0.0" "vcf-report-2.5.2")
   images+=("annotsv-3.0.9")
   images+=("vep-104.3")
 
