9823ef0c9e7ee0e14fc8b080de86c931aeb4b2af
Dennis Hendriksen <d.hendriksen@umcg.nl>
2022-01-12 07:53:27 +0100

    update gatk 4.2.2.0 to 4.2.4.1

--- install.sh
+++ install.sh
@@ -129,7 +129,7 @@ download_images () {
   local files=()
   files+=("annotsv-3.0.9.sif")
   files+=("bcftools-1.14.sif")
-  files+=("gatk-4.2.2.0.sif")
+  files+=("gatk-4.2.4.1.sif")
   files+=("vcf-decision-tree-2.0.0.sif")
   files+=("vcf-inheritance-matcher-2.0.0.sif")
   files+=("vcf-report-3.0.1.sif")
