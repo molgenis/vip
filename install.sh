7b14b74bd7897cca83ec54bba533ea88b08d068a
Dennis Hendriksen <d.hendriksen@umcg.nl>
2022-02-18 07:18:01 +0100

    fix: update capice v3.0.0rc4 to v3.0.0 (fix install script)

--- install.sh
+++ install.sh
@@ -138,7 +138,7 @@ download_images() {
   local files=()
   files+=("annotsv-3.0.9.sif")
   files+=("bcftools-1.14.sif")
-  files+=("capice-3.0.0rc4.sif")
+  files+=("capice-3.0.0.sif")
   files+=("gatk-4.2.5.0.sif")
   files+=("vcf-decision-tree-2.2.0.sif")
   files+=("vcf-inheritance-matcher-2.0.0.sif")
