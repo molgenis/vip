8fe3b98411b6e003c2e8d3c6eda5410b3e0e2a7e
Bart Charbon <b.charbon@umcg.nl>
2022-03-09 11:12:04 +0100

    bump decision tree version to v2.2.1 (#243)
    * bump decision tree version to v2.2.1
    
    * Fix thresholds for lp and lb
    
    * Fix thresholds for lp_b38 and lb_b38
    
    Co-authored-by: bartcharbon <bart.charbon@gmail.com>

--- test/test.sh
+++ test/test.sh
@@ -178,7 +178,7 @@ test_lp () {
     return 1
   fi
 
-  if [ "$(zcat "${OUTPUT_DIR}/lp.vcf.gz" | grep -vc "^#")" -lt 2455 ]; then
+  if [ "$(zcat "${OUTPUT_DIR}/lp.vcf.gz" | grep -vc "^#")" -lt 2423 ]; then
     return 1
   fi
 }
@@ -197,7 +197,7 @@ test_lp_b38 () {
     return 1
   fi
 
-  if [ "$(zcat "${OUTPUT_DIR}/lp_b38.vcf.gz" | grep -vc "^#")" -lt 2452 ]; then
+  if [ "$(zcat "${OUTPUT_DIR}/lp_b38.vcf.gz" | grep -vc "^#")" -lt 2418 ]; then
     return 1
   fi
 }
@@ -216,7 +216,7 @@ test_lb () {
     return 1
   fi
 
-  if [ "$(zcat "${OUTPUT_DIR}/lb.vcf.gz" | grep -vc "^#")" -gt 793 ]; then
+  if [ "$(zcat "${OUTPUT_DIR}/lb.vcf.gz" | grep -vc "^#")" -gt 1417 ]; then
     return 1
   fi
 }
@@ -235,7 +235,7 @@ test_lb_b38 () {
     return 1
   fi
 
-  if [ "$(zcat "${OUTPUT_DIR}/lb_b38.vcf.gz" | grep -vc "^#")" -gt 683 ]; then
+  if [ "$(zcat "${OUTPUT_DIR}/lb_b38.vcf.gz" | grep -vc "^#")" -gt 1229 ]; then
     return 1
   fi
 }
