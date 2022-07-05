7387e26c467b72a4e8c84aa6543b494e16cc3b84
bartcharbon <bart.charbon@gmail.com>
2022-07-05 14:36:12 +0200

    fix 'label' and "path" arguments for decision tree are flag arguments

--- templates/classify.sh
+++ templates/classify.sh
@@ -12,8 +12,13 @@ classify () {
   args+=("-jar" "/opt/vcf-decision-tree/lib/vcf-decision-tree.jar")
   args+=("--input" "!{vcfPath}")
   args+=("--config" "!{params.classify_decision_tree}")
-  args+=("--labels" "!{params.classify_annotate_labels}")
-  args+=("--path" "!{params.classify_annotate_path}")
+  if [ !{params.classify_annotate_labels} -eq 1 ]; then
+    args+=("--labels")
+  fi
+  if [ !{params.classify_annotate_path} -eq 1 ]; then
+    args+=("--path")
+  fi
+
   args+=("--output" "!{vcfClassifiedPath}")
 
   !{singularity_vcfdecisiontree} java "${args[@]}"
