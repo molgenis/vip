2a2bb07b0744e467e52d6d2fa184531488148139
JonathanKlimp <78917640+JonathanKlimp@users.noreply.github.com>
2022-11-02 17:59:38 +0100

    Uncommented fathmMKL for testing

--- templates/annotate.sh
+++ templates/annotate.sh
@@ -190,9 +190,9 @@ vep() {
   args+=("--custom" "!{dnaseRegionsPath},region,bed,overlap,0")
   args+=("--custom" "!{tfbsRegionsPath},region,bed,overlap,0")
   args+=("--custom" "!{ucneRegionsPath},region,bed,overlap,0")
-  #args+=("--custom" "!{fathmmMKLScoresPath},fathmm,vcf,overlap,0")
+  args+=("--custom" "!{fathmmMKLScoresPath},fathmm,vcf,overlap,0")
   args+=("--custom" "!{ncErScoresPath},ncER,bed,overlap,0")
-  args+=("--custom" "!{reMMScoresPath},ReMM,bed,overlap,0")
+  #args+=("--custom" "!{reMMScoresPath},ReMM,bed,overlap,0")
 
   if [ -n "!{vepPluginArtefact}" ]; then
     args+=("--plugin" "Artefact,!{vepPluginArtefact}")
