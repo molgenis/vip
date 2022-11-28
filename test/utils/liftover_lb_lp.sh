15e7550509561b39a62490c9f9cc70cde9172970
bartcharbon <bart.charbon@gmail.com>
2022-11-28 13:17:29 +0100

    Use apptainer instead of singularity

--- test/utils/liftover_lb_lp.sh
+++ test/utils/liftover_lb_lp.sh
@@ -19,10 +19,10 @@ fi
 VIP_DIR=$1;
 BIND=$2;
 
-SINGULARITY_BIND=${BIND} singularity exec ${VIP_DIR}/images/bcftools-1.14.sif bcftools convert -O z -o ${VIP_DIR}/test/resources/lb.vcf.gz ${VIP_DIR}/test/resources/lb.bcf.gz
+APPTAINER_BIND=${BIND} apptainer exec ${VIP_DIR}/images/bcftools-1.14.sif bcftools convert -O z -o ${VIP_DIR}/test/resources/lb.vcf.gz ${VIP_DIR}/test/resources/lb.bcf.gz
 
-SINGULARITY_BIND=${BIND} singularity exec ${VIP_DIR}/images/picard-2.26.11.sif java -jar /opt/picard/lib/picard.jar LiftoverVcf I="${VIP_DIR}/test/resources/lb.vcf.gz" O="${VIP_DIR}/test/resources/lb_b38.vcf.gz" CHAIN="${VIP_DIR}/test/resources/b37ToHg38.over.chain" REJECT="${VIP_DIR}/test/resources/lb_b38_rejected.vcf.gz" WARN_ON_MISSING_CONTIG=true R="${VIP_DIR}/resources/GRCh38/GCA_000001405.15_GRCh38_no_alt_analysis_set.fna.gz"
+APPTAINER_BIND=${BIND} apptainer exec ${VIP_DIR}/images/picard-2.26.11.sif java -jar /opt/picard/lib/picard.jar LiftoverVcf I="${VIP_DIR}/test/resources/lb.vcf.gz" O="${VIP_DIR}/test/resources/lb_b38.vcf.gz" CHAIN="${VIP_DIR}/test/resources/b37ToHg38.over.chain" REJECT="${VIP_DIR}/test/resources/lb_b38_rejected.vcf.gz" WARN_ON_MISSING_CONTIG=true R="${VIP_DIR}/resources/GRCh38/GCA_000001405.15_GRCh38_no_alt_analysis_set.fna.gz"
 
-SINGULARITY_BIND=${BIND} singularity exec ${VIP_DIR}/images/bcftools-1.14.sif bcftools convert -O b -o ${VIP_DIR}/test/resources/lb_b38.bcf.gz ${VIP_DIR}/test/resources/lb_b38.vcf.gz
+APPTAINER_BIND=${BIND} apptainer exec ${VIP_DIR}/images/bcftools-1.14.sif bcftools convert -O b -o ${VIP_DIR}/test/resources/lb_b38.bcf.gz ${VIP_DIR}/test/resources/lb_b38.vcf.gz
 
-SINGULARITY_BIND=${BIND} singularity exec ${VIP_DIR}/images/picard-2.26.11.sif java -jar /opt/picard/lib/picard.jar LiftoverVcf I="${VIP_DIR}/test/resources/lp.vcf.gz" O="${VIP_DIR}/test/resources/lp_b38.vcf.gz" CHAIN="${VIP_DIR}/test/resources/b37ToHg38.over.chain" REJECT="${VIP_DIR}/test/resources/lp_b38_rejected.vcf" WARN_ON_MISSING_CONTIG=true R="${VIP_DIR}/resources/GRCh38/GCA_000001405.15_GRCh38_no_alt_analysis_set.fna.gz"
+APPTAINER_BIND=${BIND} apptainer exec ${VIP_DIR}/images/picard-2.26.11.sif java -jar /opt/picard/lib/picard.jar LiftoverVcf I="${VIP_DIR}/test/resources/lp.vcf.gz" O="${VIP_DIR}/test/resources/lp_b38.vcf.gz" CHAIN="${VIP_DIR}/test/resources/b37ToHg38.over.chain" REJECT="${VIP_DIR}/test/resources/lp_b38_rejected.vcf" WARN_ON_MISSING_CONTIG=true R="${VIP_DIR}/resources/GRCh38/GCA_000001405.15_GRCh38_no_alt_analysis_set.fna.gz"
