475382b173b84b7f7f3cae00ab7095ebac539865
Dennis Hendriksen <d.hendriksen@umcg.nl>
2022-12-16 07:40:03 +0000

    add --profile to vip.sh

--- vip.sh
+++ vip.sh
@@ -3,10 +3,11 @@ SCRIPT_NAME="$(basename "$0")"
 SCRIPT_DIR=$(dirname "$(realpath "$0")")
 
 usage() {
-  echo -e "usage: ${SCRIPT_NAME} [-i <arg> -o <arg> -w <arg>]
+  echo -e "usage: ${SCRIPT_NAME} [-w <arg> -i <arg> -o <arg>]
   -w, --workflow <arg>  workflow to execute. allowed values: cram, fastq, gvcf, vcf
   -i, --input    <arg>  path to sample sheet .tsv
   -o, --output   <arg>  output folder
+  -p, --profile  <arg>  nextflow configuration profile (optional)
   -c, --config   <arg>  path to additional nextflow .cfg (optional)
   -h, --help            print this message and exit"
 }
@@ -16,6 +17,7 @@ validate() {
   local -r input="${2}"
   local -r output="${3}"
   local -r config="${4}"
+  local -r profile="${5}"
   
   if [[ -z "${workflow}" ]]; then
     >&2 echo -e "error: missing required -w / --workflow"
@@ -38,6 +40,9 @@ validate() {
     usage
     exit 2
   fi
+  if [[ -n "${config}" ]] && [[ ! -f "${config}" ]]; then
+    >&2 echo -e "error: config '${config}' does not exist"
+  fi
 }
 
 execute_workflow() {
@@ -45,6 +50,7 @@ execute_workflow() {
   local -r paramInput="${2}"
   local -r paramOutput="${3}"
   local -r paramConfig="${4}"
+  local -r paramProfile="${5}"
 
   rm -f "${paramOutput}/nxf_report.html"
   rm -f "${paramOutput}/nxf_timeline.html"
@@ -71,7 +77,7 @@ execute_workflow() {
   "${SCRIPT_DIR}/nextflow" -C "${configs[@]}" -log "${paramOutput}/.nxf.log" run "${SCRIPT_DIR}/vip_${paramWorkflow}.nf" \
     -offline \
     -resume \
-    -profile cluster \
+    -profile ${paramProfile} \
     -with-report "${paramOutput}/nxf_report.html" \
     -with-timeline "${paramOutput}/nxf_timeline.html" \
     --input "${paramInput}" \
@@ -79,7 +85,7 @@ execute_workflow() {
 }
 
 main() {
-  local -r args=$(getopt -a -n pipeline -o w:i:o:c:h --long workflow:,input:,output:,config:,help -- "$@")
+  local -r args=$(getopt -a -n pipeline -o w:i:o:c:p:h --long workflow:,input:,output:,config:,profile:,help -- "$@")
   # shellcheck disable=SC2181
   if [[ $? != 0 ]]; then
     usage
@@ -90,6 +96,12 @@ main() {
   local input=""
   local output=""
   local config=""
+  local profile=""
+  if command -v sbatch &> /dev/null; then
+    profile="slurm"
+  else
+    profile="local"
+  fi
 
   eval set -- "${args}"
   while :; do
@@ -115,6 +127,10 @@ main() {
       config="$2"
       shift 2
       ;;
+    -p | --profile)
+      profile="$2"
+      shift 2
+      ;;
     --)
       shift
       break
@@ -126,8 +142,8 @@ main() {
     esac
   done
 
-  validate "${workflow}" "${input}" "${output}" "${config}"
-  execute_workflow "${workflow}" "${input}" "${output}" "${config}"
+  validate "${workflow}" "${input}" "${output}" "${config}" "${profile}"
+  execute_workflow "${workflow}" "${input}" "${output}" "${config}" "${profile}"
 }
 
 main "${@}"
