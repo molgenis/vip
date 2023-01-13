e960b2d51346f46429719a43f751085be8808915
bartcharbon <b.charbon@umcg.nl>
2023-01-13 07:02:13 +0000

    refactor tests to allow separate vcf and cram runs

--- test/test_vcf.sh
+++ test/test_vcf.sh
@@ -2,6 +2,26 @@
 
 source ./test_utils.sh
 
+test_gvcf () {
+  echo -e "params { vcf.filter.classes = \"LQ,B,LB,VUS,LP,P\" }" > "${OUTPUT_DIR}/custom.cfg"
+
+  local args=()
+  args+=("--workflow" "vcf")
+  args+=("--config" "${OUTPUT_DIR}/custom.cfg")
+  args+=("--input" "${TEST_RESOURCES_DIR}/gvcf.tsv")
+  args+=("--output" "${OUTPUT_DIR}")
+  args+=("--profile" "local")
+  args+=("--assembly" "GRCh38")
+
+  if ! "${CMD_VIP}" "${args[@]}" > /dev/null 2>&1; then
+    return 1
+  fi
+
+  if [ ! "$(zcat "${OUTPUT_DIR}/vip.vcf.gz" | grep -vc "^#")" -eq 1 ]; then
+    return 1
+  fi
+}
+
 test_empty_input () {
   local args=()
   args+=("--workflow" "vcf")
@@ -243,6 +263,11 @@ test_lb_b38 () {
 
 run_tests () {
   before_all
+
+  TEST_ID="gvcf"
+  before_each
+  test_gvcf
+  after_each
 
   TEST_ID="test_empty_input"
   before_each
