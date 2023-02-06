568bce8663fb69d49946c7fc4c3931c5f33bf4f9
dennishendriksen <d.hendriksen@umcg.nl>
2023-02-06 11:50:22 +0000

    fix:revert capice v5.0.0 workaround

--- resources/vep/plugins/Capice.pm
+++ resources/vep/plugins/Capice.pm
@@ -85,10 +85,10 @@ sub parse_file_header {
         if ($tokens[$i] eq "pos") {
             $col_idx->{idx_pos} = $i;
         }
-        if ($tokens[$i] eq "REF") {
+        if ($tokens[$i] eq "ref") {
             $col_idx->{idx_ref} = $i;
         }
-        if ($tokens[$i] eq "ALT") {
+        if ($tokens[$i] eq "alt") {
             $col_idx->{idx_alt} = $i;
         }
         if ($tokens[$i] eq "feature_type") {
