b61de8d4d3f60fe94a8a55b721f26534c0dd7684
Sander van den Hoek <savandenhoek@gmail.com>
2023-03-28 13:18:30 +0200

    Fixed slurm check to work on non-slurm runs.

--- utils/apptainer/build.sh
+++ utils/apptainer/build.sh
@@ -1,9 +1,10 @@
 #!/bin/bash
-set -euo pipefail
+set -eo pipefail
 
 # Retrieve directory containing the collection of scripts (allows using other scripts with & without Slurm).
 if [[ -n "${SLURM_JOB_ID}" ]]; then SCRIPT_DIR=$(dirname "$(scontrol show job "${SLURM_JOB_ID}" | awk -F= '/Command=/{print $2}' | cut -d ' ' -f 1)"); else SCRIPT_DIR=$(dirname "$(realpath "$0")"); fi
 SCRIPT_NAME="$(basename "$0")"
+set -u
 
 usage() {
   echo -e "usage: ${SCRIPT_NAME} -f
