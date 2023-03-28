31f36539cf3e95e2a5c099132e3310eefa5e6415
bartcharbon <b.charbon@umcg.nl>
2023-03-28 11:16:17 +0000

    Fix tests

--- modules/cram/templates/publish.sh
+++ modules/cram/templates/publish.sh
@@ -8,15 +8,29 @@ concat () {
   args+=("--output" "!{vcfOut}")
   args+=("--no-version")
   args+=("--threads" "!{task.cpus}")
+  for vcf in !{vcfs}
+  do
+    args+=("sorted_${vcf}")
+  done
 
-  ${CMD_BCFTOOLS} "${args[@]}" !{vcfs}
+  ${CMD_BCFTOOLS} "${args[@]}"
 }
 
 index () {
   ${CMD_BCFTOOLS} index --csi --output "!{vcfOutIndex}" --threads "!{task.cpus}" "!{vcfOut}"
 }
 
+order_samples () {
+  for vcf in !{vcfs}
+  do
+    ${CMD_BCFTOOLS} query -l ${vcf} | sort > sorted_samples.txt
+    ${CMD_BCFTOOLS} view -O z -S "sorted_samples.txt" ${vcf} > "sorted_${vcf}" 
+    ${CMD_BCFTOOLS} index --csi --output "sorted_${vcf}.csi" --threads "!{task.cpus}" "sorted_${vcf}"
+  done
+}
+
 main() {
+  order_samples
   concat
   index
 }
--- test/test_cram.sh
+++ test/test_cram.sh
@@ -68,7 +68,7 @@ test_cram_multiproject () {
 
 test_cram_trio () {
   echo -e "params { vcf.filter.classes = \"LQ,B,LB,VUS,LP,P\"\nvcf.filter_samples.classes = \"LQ,MV,OK\" }" > "${OUTPUT_DIR}/custom.cfg"
-  ln -s ${TEST_RESOURCES_DIR}/test.cram ${TEST_RESOURCES_DIR}/test2.cram
+  ln -sf ${TEST_RESOURCES_DIR}/test1.cram ${TEST_RESOURCES_DIR}/test2.cram
 
   local args=()
   args+=("--workflow" "cram")
