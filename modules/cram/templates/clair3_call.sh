ffa5fc919e9a1c322947baaef8085f0ebefe45ed
Dennis Hendriksen <d.hendriksen@umcg.nl>
2023-04-03 09:08:48 +0000

    reduce intermediate file space usaga

--- modules/cram/templates/clair3_call.sh
+++ modules/cram/templates/clair3_call.sh
@@ -8,8 +8,12 @@ create_bed () {
 # workaround for clair 3 issue where the ebi server is called to decode the cram
 # https://github.com/HKU-BAL/Clair3/issues/180
 convert_to_bam () {
-  ${CMD_SAMTOOLS} view --reference "!{reference}" --bam --output "!{cram}.bam" --threads "!{task.cpus}" "!{cram}"
+  ${CMD_SAMTOOLS} view --reference "!{reference}" --bam --regions-file "!{bed}" --output "!{cram}.bam" --threads "!{task.cpus}" "!{cram}"
   ${CMD_SAMTOOLS} index "!{cram}.bam"
+}
+
+convert_to_bam_cleanup () {
+  rm "!{cram}.bam" "!{cram}.bam.bai"
 }
 
 call_small_variants () {
@@ -23,6 +27,7 @@ call_small_variants () {
     args+=("--output=$(realpath .)")
     args+=("--sample_name=!{meta.sample.individual_id}")
     args+=("--longphase_for_phasing")
+    args+=("--remove_intermediate_dir")
 
     # Prevent Clair3 writing in home directory via samtools (https://www.htslib.org/doc/samtools.html#ENVIRONMENT_VARIABLES)
     XDG_CACHE_HOME=$(realpath .) ${CMD_CLAIR3} "${args[@]}"
@@ -40,6 +45,7 @@ main() {
     create_bed
     convert_to_bam
     call_small_variants
+    convert_to_bam_cleanup
     index
 }
 
