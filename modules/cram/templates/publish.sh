99fe3d0408ea3ef1be2355f5183078a55e96c9cd
bartcharbon <b.charbon@umcg.nl>
2023-04-03 10:13:28 +0000

    fix for loops

--- modules/cram/templates/publish.sh
+++ modules/cram/templates/publish.sh
@@ -11,7 +11,8 @@ concat () {
   local -a vcf_array=(!{vcfs})
   for (( i=0; i<${#vcf_array[@]}; i++ ));
   do
-    args+=("sorted_${vcf_array[$i]}")
+      vcf="${vcf_array["${i}"]}"
+    args+=("sorted_${vcf}")
   done
 
   ${CMD_BCFTOOLS} "${args[@]}"
@@ -25,7 +26,7 @@ order_samples () {
   local -a vcf_array=(!{vcfs})
   for (( i=0; i<${#vcf_array[@]}; i++ ));
   do
-    vcf=${vcf_array[$i]}
+    vcf="${vcf_array["${i}"]}"
     ${CMD_BCFTOOLS} query --list-samples ${vcf} | sort > sorted_samples.txt
     ${CMD_BCFTOOLS} view --no-version --threads "!{task.cpus}" --output-type "z" --samples-file "sorted_samples.txt" ${vcf} > "sorted_${vcf}" 
     ${CMD_BCFTOOLS} index --csi --output "sorted_${vcf}.csi" --threads "!{task.cpus}" "sorted_${vcf}"
