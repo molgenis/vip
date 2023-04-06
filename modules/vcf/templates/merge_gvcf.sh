6dfeccec24eeb1083e116d17bb3b1cd60c5f3f10
Dennis Hendriksen <d.hendriksen@umcg.nl>
2023-04-06 06:26:34 +0000

    Fix #344 merge_vcf proces can fail with GLnexus merge error

--- modules/vcf/templates/merge_gvcf.sh
+++ modules/vcf/templates/merge_gvcf.sh
@@ -1,12 +1,28 @@
 #!/bin/bash
 set -euo pipefail
 
+# workaround for https://github.com/dnanexus-rnd/GLnexus/issues/238
+reheader () {
+  for gVcf in !{gVcfs}; do
+    ${CMD_BCFTOOLS} reheader --fai "!{refSeqFaiPath}" --output "reheadered_${gVcf}" --threads "!{task.cpus}" "${gVcf}"
+  done
+}
+
+# cannot use --bed because it is broken: https://github.com/dnanexus-rnd/GLnexus/issues/279
 merge () {
+  local gVcfsReheadered=()
+  for gVcf in !{gVcfs}; do
+    gVcfsReheadered+=("reheadered_${gVcf}")
+  done
+
   local args=()
   args+=("--dir" "glnexus")
   args+=("--config" "!{config}")
   args+=("--threads" "!{task.cpus}")
-  ${CMD_GLNEXUS} "${args[@]}" !{gVcfs} | ${CMD_BCFTOOLS} view --output-type z --output-file "!{vcfOut}" --no-version --threads "!{task.cpus}"
+  for gVcf in !{gVcfs}; do
+    args+=("reheadered_${gVcf}")
+  done
+  ${CMD_GLNEXUS} "${args[@]}" | ${CMD_BCFTOOLS} view --output-type z --output-file "!{vcfOut}" --no-version --threads "!{task.cpus}"
 }
 
 index () {
@@ -14,8 +30,16 @@ index () {
   ${CMD_BCFTOOLS} index --stats "!{vcfOut}" > "!{vcfOutStats}"
 }
 
+reheader_cleanup () {
+  for gVcf in !{gVcfs}; do
+    rm "reheadered_${gVcf}"
+  done
+}
+
 main () {
+  reheader
   merge
+  reheader_cleanup
   index
 }
 
