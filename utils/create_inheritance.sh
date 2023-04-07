2d8f67dce41230f6d0859fe67a5f6a8f61f4b331
bart.charbon <b.charbon@umcg.nl>
2023-04-07 10:24:37 +0200

    Fix inheritance: add option to provide genemap2

--- utils/create_inheritance.sh
+++ utils/create_inheritance.sh
@@ -4,7 +4,51 @@
 if [[ -n "${SLURM_JOB_ID}" ]]; then SCRIPT_DIR=$(dirname "$(scontrol show job "${SLURM_JOB_ID}" | awk -F= '/Command=/{print $2}' | cut -d ' ' -f 1)"); else SCRIPT_DIR=$(dirname "$(realpath "$0")"); fi
 SCRIPT_NAME="$(basename "$0")"
 
+usage() {
+  echo -e "usage: ${SCRIPT_NAME} [-i <arg>]
+  -i, --input <arg>  path to omim genemap2 file
+
+  If no parameters are provided the inheritance file is created without OMIM."
+}
+
+createDummyGenemap() {
+  echo "create dummy ${geneMapFilePath}"
+  cat >"${geneMapFilePath}" <<EOT
+# header line 1
+# header line 2
+# header line 3
+# Chromosome	Genomic	Position	Start	Genomic	Position	End	Cyto	Location	Computed	Cyto	Location	MIM Number	Gene Symbols	Gene Name	Approved Symbol	Entrez Gene ID	Ensembl Gene ID	Comments	Phenotypes	Mouse Gene Symbol/ID
+EOT
+  echo "create dummy ${geneMapFilePath} done"
+}
+
 main() {
+  local args=$(getopt -a -n pipeline -o ih --long input_omim:,help -- "$@")
+
+  local geneMapFilePath
+  echo "getopts"
+  eval set -- "${args}"
+  while :; do
+    case "$1" in
+    -h | --help)
+      usage
+      exit 0
+      ;;
+    -i | --input_omim)
+      geneMapFilePath="${2}"
+      shift 2
+      ;;
+    --)
+      shift
+      break
+      ;;
+    *)
+      usage
+      exit 2
+      ;;
+    esac
+  done
+
   echo -e "downloading ..."
   wget --quiet --continue https://download.molgeniscloud.org/downloads/vip/images/utils/vcf-inheritance-3.0.1.sif
   wget --quiet --continue https://download.molgeniscloud.org/downloads/vip/resources/utils/incomplete_penetrantie_genes_entrez_20210125.tsv
@@ -12,20 +56,19 @@ main() {
   wget --quiet --continue https://research.nhgri.nih.gov/CGD/download/txt/CGD.txt.gz
   echo -e "downloading done"
 
-  # create dummy genemap2.txt
-  local geneMapFilePath="genemap2.txt"
-  cat >"${geneMapFilePath}" <<EOT
-# header line 1
-# header line 2
-# header line 3
-# Chromosome	Genomic	Position	Start	Genomic	Position	End	Cyto	Location	Computed	Cyto	Location	MIM Number	Gene Symbols	Gene Name	Approved Symbol	Entrez Gene ID	Ensembl Gene ID	Comments	Phenotypes	Mouse Gene Symbol/ID
-EOT
+  # create dummy genemap2.txt if not provided
+  if [ -z "${geneMapFilePath}" ]; then
+    geneMapFilePath="genemap2.txt"
+    createDummyGenemap "${geneMapFilePath}"
+  else
+    echo "genemap2 file provided, skipping dummy file creation."
+  fi
 
   local outputPath="inheritance_$(date '+%Y%m%d').tsv"
 
   local args=()
   args+=("-jar" "/opt/vcf-inheritance/lib/genemap-mapper.jar")
-  args+=("-i" "genemap2.txt")
+  args+=("-i" "${geneMapFilePath}")
   args+=("-h" "phenotype.hpoa")
   args+=("--incomplete_penetrance" "incomplete_penetrantie_genes_entrez_20210125.tsv")
   args+=("-c" "CGD.txt.gz")
@@ -33,7 +76,7 @@ EOT
   args+=("-f")
 
   echo -e "creating ${outputPath} ..."
-  APPTAINER_BIND="${SCRIPT_DIR}" apptainer exec vcf-inheritance-3.0.1.sif java "${args[@]}"
+  APPTAINER_BIND="${SCRIPT_DIR},${geneMapFilePath}" apptainer exec vcf-inheritance-3.0.1.sif java "${args[@]}"
   echo -e "creating ${outputPath} done"
 }
 
