3f281fcad2c04b77825855d0de34a31deb01b34b
Dennis Hendriksen <d.hendriksen@umcg.nl>
2023-04-13 15:46:11 +0200

    samtools_addreplacerg: reduce intermediate files (#350)

--- modules/cram/templates/samtools_addreplacerg.sh
+++ modules/cram/templates/samtools_addreplacerg.sh
@@ -9,36 +9,24 @@ create_cram_slice () {
   local args=()
   args+=("view")
   args+=("--cram")
-  args+=("--output" "!{cram.simpleName}_sliced.cram")
   args+=("--target-file" "!{bed}")
   args+=("--reference" "!{reference}")
-  args+=("--write-index")
   args+=("--no-PG")
   args+=("--threads" "!{task.cpus}")
   args+=("!{cram}")
 
-  ${CMD_SAMTOOLS} "${args[@]}"
-}
-
-add_replace_read_group () {
-  tab=$'\t';
-  ${CMD_SAMTOOLS} addreplacerg -r "@RG${tab}ID:!{meta.sample.individual_id}${tab}SM:!{meta.sample.individual_id}" -o "!{cramOut}" --threads "!{task.cpus}" "!{cram.simpleName}_sliced.cram"
+  local -r tab=$'\t';
+  ${CMD_SAMTOOLS} "${args[@]}" | ${CMD_SAMTOOLS} addreplacerg -r "@RG${tab}ID:!{meta.sample.individual_id}${tab}SM:!{meta.sample.individual_id}" -o "!{cramOut}" --threads "!{task.cpus}" -
 }
 
 index () {
   ${CMD_SAMTOOLS} index "!{cramOut}"
 }
 
-create_cram_slice_cleanup () {
-  rm "!{cram.simpleName}_sliced.cram" "!{cram.simpleName}_sliced.cram.crai"
-}
-
 main() {  
-  create_bed  
+  create_bed
   create_cram_slice
-  add_replace_read_group
   index
-  create_cram_slice_cleanup
 }
 
 main "$@"
