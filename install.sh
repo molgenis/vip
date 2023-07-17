47522514758725a69681e9d8f8d2c278c280ae92
bartcharbon <b.charbon@umcg.nl>
2023-07-17 12:47:26 +0000

    add clinical repeats bed file

--- install.sh
+++ install.sh
@@ -80,6 +80,7 @@ download_resources_molgenis() {
 
   if [ "${assembly}" == "ALL" ] || [ "${assembly}" == "GRCh38" ]; then
     files+=("GRCh38/capice_model_v5.1.1-v1.ubj")
+    files+=("GRCh38/clinical_repeats.bed")
     files+=("GRCh38/clinvar_20230604.vcf.gz")
     files+=("GRCh38/clinvar_20230604.vcf.gz.tbi")
     files+=("GRCh38/expansionhunter_variant_catalog.json")
