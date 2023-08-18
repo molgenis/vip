ed43c767a26de42f06646a5761e9617ee2bcf273
bartcharbon <b.charbon@umcg.nl>
2023-08-18 09:34:53 +0000

    Add Stranger for str annotation

--- modules/vcf/templates/annotate.sh
+++ modules/vcf/templates/annotate.sh
@@ -164,6 +164,17 @@ capice_predict() {
   fi
 }
 
+stranger() {
+    cp !{vcfOut} stranger_input.vcf.gz
+
+    local args=()
+    args+=("-f" "!{strangerCatalog}")
+    args+=("stranger_input.vcf.gz")
+
+    ${CMD_STRANGER} "${args[@]}" | ${CMD_BGZIP} -c > "!{vcfOut}"
+    rm "stranger_input.vcf.gz"
+}
+
 vep() {
   local args=()
   args+=("--input_file" "!{vcf}")
@@ -243,6 +254,9 @@ main () {
   fi
   capice
   vep
+  if [ -n "!{strangerCatalog}" ]; then
+    stranger
+  fi
   index
 }
 
