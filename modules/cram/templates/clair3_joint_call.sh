b139460c9463fe596dcfdf3d092e72ac9c790031
dennishendriksen <d.hendriksen@umcg.nl>
2023-08-31 15:54:13 +0200

    fix GLnexus memory usage: reduce limit with 1GB

--- modules/cram/templates/clair3_joint_call.sh
+++ modules/cram/templates/clair3_joint_call.sh
@@ -19,7 +19,7 @@ merge () {
   args+=("--dir" "glnexus")
   args+=("--config" "!{config}")
   args+=("--threads" "!{task.cpus}")
-  args+=("--mem-gbytes" "!{task.memory.toGiga()}")
+  args+=("--mem-gbytes" "!{task.memory.toGiga() - 1}")
   for gVcf in !{gVcfs}; do
     args+=("reheadered_${gVcf}")
   done
