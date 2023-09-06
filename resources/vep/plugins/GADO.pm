30c863b05ba6eb47936914884010f879eb8fe390
Dennis Hendriksen <d.hendriksen@umcg.nl>
2023-09-06 08:43:34 +0000

    Fix GADO warning, small improvements

--- resources/vep/plugins/GADO.pm
+++ resources/vep/plugins/GADO.pm
@@ -9,13 +9,14 @@ use base qw(Bio::EnsEMBL::Variation::Utils::BaseVepPlugin);
 =head1 NAME
  GADO
 =head1 SYNOPSIS
- 
+ mv GADO.pm ~/.vep/Plugins
+ ./vep -i variations.vcf --plugin GADO,gado/all_samples.txt,ensembl_ncbi_gene_id_mapping.tsv
 =head1 DESCRIPTION
  Plugin to annotate consequences with GADO scores modes based on their gene.
 =cut
 
 sub version {
-    return '0.1';
+    return '0.2';
 }
 
 sub feature_types {
@@ -26,15 +27,14 @@ sub variant_feature_types {
     return ['VariationFeature'];
 }
 
-my $self;
-
 sub get_header_info {
-    $self = GADO->new;
-    my $result;
-    $result->{GADO_SC} = "The combined prioritization GADO Z-score over the supplied HPO terms for this case.";
-    $result->{GADO_PD} = "The GADO predicion for the relation between the phenotypes and the gene, HC: high confidence, LC: low confidence.";
-    return $result;
+    return {
+        GADO_SC => "The combined prioritization GADO Z-score over the supplied HPO terms for this case.",
+        GADO_PD => "The GADO predicion for the relation between the phenotypes and the gene, HC: high confidence, LC: low confidence."
+    };
 }
+
+my $self;
 
 sub new {
     if (!(defined $self)) {
@@ -81,7 +81,7 @@ sub parseMappingFile {
         my $line = $_;
         chomp($line);
         @split = split(/\t/, $line);
-        if (defined $split[0] and length $split[0]){
+        if (defined $split[0] and length $split[0] and defined $split[1] and length $split[1]){
             $mapping_data{$split[1]} = $split[0];
         }
     }
@@ -90,28 +90,25 @@ sub parseMappingFile {
 
 sub run {
     my ($self, $transcript_variation_allele) = @_;
-    my %gene_data = %{$self->{gene_data}};
-    my %gene_mapping = %{$self->{gene_mapping}};
+
     my $transcript = $transcript_variation_allele->transcript;
     return {} unless ($transcript->{_gene_symbol_source} eq "EntrezGene");
+
     my $entrez_gene_id = $transcript->{_gene_stable_id};
     return {} unless $entrez_gene_id;
-    my $ensembl_gene_id = $gene_mapping{$entrez_gene_id};
-    unless ($ensembl_gene_id){
-        return {};
-    }
-    my $result;
-    my $gene_value = $gene_data{$ensembl_gene_id};
-    return {} unless $gene_value;
 
-    my %gene_hash = %{$gene_value};
-    my $score = $gene_hash{Zscore};
-    $result->{GADO_SC} = $score;
-    if($score >= 5){
-        $result->{GADO_PD} = "HC";
-    } elsif($score >= 3){
-        $result->{GADO_PD} = "LC";
-    }
-    return $result;
+    my $ensembl_gene_id = $self->{gene_mapping}->{$entrez_gene_id};
+    return {} unless $ensembl_gene_id;
+
+    my $gene_value = $self->{gene_data}->{$ensembl_gene_id};
+    return {} unless $gene_value;
+    
+    my $score = $gene_value->{Zscore};
+    return {} unless $score;
+
+    return {
+        GADO_SC => $score,
+        GADO_PD => $score >= 5 ? "HC" : ($score >= 3 ? "LC" : undef)
+    };
 }
 1;
