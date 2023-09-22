84cc98949115a383bb6d7cfbdffb28a86811e5be
bartcharbon <b.charbon@umcg.nl>
2023-09-22 11:09:17 +0000

    fix deepvariant config

--- utils/apptainer/build.sh
+++ utils/apptainer/build.sh
@@ -104,7 +104,7 @@ main() {
 
   declare -A uris
   uris["docker://ensemblorg/ensembl-vep:release_109.3"]="vep-109.3"
-  uris["docker://google/deepvariant:1.5.0"]="deepvariant-v1.5.0"
+  uris["docker://google/deepvariant:1.5.0"]="deepvariant-1.5.0"
   
   for i in "${!uris[@]}"; do
     echo "---Building from URI ${i}---"
