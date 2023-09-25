28929605839cfc82a46c5a9f618e2aae6519d52d
Dennis Hendriksen <d.hendriksen@umcg.nl>
2023-09-25 08:10:07 +0000

    add GIAB illumina WES and AshkenazimTrio_pacbio_ccs tests
    add util function: download
    
    fix
    
    add GIAB AshkenazimTrio_pacbio_ccs test
    
    giab pacbio-hifi bam test
    
    fix:enable test

--- test/test_utils.sh
+++ test/test_utils.sh
@@ -68,6 +68,33 @@ after_all () {
   fi
 }
 
+# arguments:
+#   $1  url
+#   $2  md5 checksum
+download() {
+  local -r url="${1}"
+  local -r md5="${2}"
+
+  local -r filename="${url##*/}"
+  local -r output_dir="${TEST_RESOURCES_DOWNLOADS_DIR}/${TEST_ID}"
+  local -r output="${output_dir}/${filename}"
+
+  if [ ! -f "${output}" ]; then
+    mkdir -p "${output_dir}"
+    if ! wget --quiet --continue "${url}" --output-document "${output}"; then
+      echo -e "an error occurred downloading ${url}"
+        # wget always writes an (empty) output file regardless of errors
+        rm -f "${output}"
+        exit 1
+    fi
+  fi
+
+  if ! echo "${md5}"  "${output_dir}/${filename}" | md5sum --check --quiet --status --strict; then
+    echo -e "checksum check failed for '${output_dir}/${filename}'"
+    exit 1
+  fi
+}
+
 download_test_resource() {
   local -r file="${1}"
 
