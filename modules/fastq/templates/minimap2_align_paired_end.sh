736abbc1b462c178974399c7b056c63f07362093
Bart Charbon <b.charbon@umcg.nl>
2023-11-03 12:04:43 +0100

    Update modules/fastq/templates/minimap2_align_paired_end.sh
    Co-authored-by: Dennis Hendriksen <d.hendriksen@umcg.nl>

--- modules/fastq/templates/minimap2_align_paired_end.sh
+++ modules/fastq/templates/minimap2_align_paired_end.sh
@@ -5,6 +5,7 @@ align() {
     local args=()
     args+=("-t" "!{task.cpus}")
     args+=("-a")
+    # MarkDuplicates uses the LB (= DNA preparation library identifier) field to determine which read groups might contain molecular duplicates, in case the same DNA library was sequenced on multiple lanes.
     args+=("-R" "@RG\tID:$(basename !{fastqR1})\tPL:!{platform}\tLB:!{sampleId}\tSM:!{sampleId}")
     args+=("-x" "sr")
     if [[ "!{softClipping}" == "true" ]]; then
