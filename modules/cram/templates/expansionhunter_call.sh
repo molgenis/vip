b1e6259f92b9b5a92e2423592fa5fc1297b64bd4
Bart Charbon <b.charbon@umcg.nl>
2023-12-14 07:41:57 +0100

    Workaround for Expansionhunter output missing SVTYPE (#539)
    * Workaround for Expansionhunter output missing SVTYPE
    
    * enable exdpansion hunter in test

--- modules/cram/templates/expansionhunter_call.sh
+++ modules/cram/templates/expansionhunter_call.sh
@@ -21,8 +21,12 @@ postprocess () {
   # workaround: ExpansionHunter extracts the sample name from the .cram, this might not be equals to the actual sample identifier
   # workaround: ExpansionHunter produces an invalid .vcf due to missing contig headers, see https://github.com/Illumina/ExpansionHunter/issues/153
   # workaround: ExpansionHunter produces an invalid .vcf due to missing headers if all calls are ./., see <TODO report issue>
+  # workaround: ExpansionHunter are missing SVTYPE INFO field, see https://github.com/Illumina/ExpansionHunter/issues/186
   echo -e "!{sampleId}" > "samples.txt"
-  ${CMD_BCFTOOLS} reheader --fai "!{paramReferenceFai}" --samples samples.txt --threads "!{task.cpus}" "short_tandem_repeats.vcf" | ${CMD_BCFTOOLS} filter --exclude "GT=\"mis\"" --output-type z --output "!{vcfOut}" --no-version --threads "!{task.cpus}"
+  ${CMD_BCFTOOLS} reheader --fai "!{paramReferenceFai}" --samples samples.txt --threads "!{task.cpus}" "short_tandem_repeats.vcf" | \
+  ${CMD_BCFTOOLS} filter --exclude "GT=\"mis\"" --no-version --threads "!{task.cpus}"| \
+  awk 'BEGIN{FS=OFS="\t"} /^#/ {print; next} {$8="SVTYPE=STR;"$8; print; next;} { print; }' | \
+  ${CMD_BGZIP} -c > "!{vcfOut}"
 }
 
 index () {
