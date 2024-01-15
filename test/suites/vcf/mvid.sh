7f1e6407b2b7a3805582bc0ab0d2706c419e6a75
dennishendriksen <d.hendriksen@umcg.nl>
2024-01-15 15:34:40 +0100

    add test: mvid

---
+++ test/suites/vcf/mvid.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+set -euo pipefail
+
+args=()
+args+=("--workflow" "vcf")
+args+=("--input" "${TEST_RESOURCES_DIR}/mvid.tsv")
+args+=("--output" "${OUTPUT_DIR}")
+args+=("--resume")
+
+vip "${args[@]}" 1> /dev/null
+
+# compare expected to actual output and store result
+if [ "$(zcat "${OUTPUT_DIR}/vip.vcf.gz" | grep -vc "^#")" -gt 510 ]; then
+  result="0"
+else
+  result="1"
+fi
+echo -n "${result}" > "${OUTPUT_DIR}/.exitcode"
+
+# always exit with success error code
+exit 0
