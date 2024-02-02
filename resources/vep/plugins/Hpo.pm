25b12401f96461ba0d78f96270153a695ebabf0e
bartcharbon <b.charbon@umcg.nl>
2024-02-02 07:10:37 +0000

    Fix the fix

--- resources/vep/plugins/Hpo.pm
+++ resources/vep/plugins/Hpo.pm
@@ -76,7 +76,7 @@ sub run {
     my ($self, $transcript_variation_allele) = @_;
 
     # fail fast: sub-class doesn't contain transcript method
-    return {} unless ($base_variation_feature_overlap_allele->can('transcript'));
+    return {} unless ($transcript_variation_allele->can('transcript'));
     my $transcript = $transcript_variation_allele->transcript;
     return {} unless ($transcript->{_gene_symbol_source} eq "EntrezGene");
 
