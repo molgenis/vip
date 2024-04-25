030f226205dd77a8f775a7b998f398029e5ec93d
bartcharbon <b.charbon@umcg.nl>
2024-04-25 14:19:26 +0000

    bump straglr

--- utils/apptainer/build.sh
+++ utils/apptainer/build.sh
@@ -93,7 +93,7 @@ main() {
   images+=("picard-3.1.1")
   images+=("samtools-1.17-patch1")
   images+=("stranger-0.8.1")
-  images+=("straglr-1.4.4_vip_v2")
+  images+=("straglr-1.4.4_vip_v3")
   images+=("vcf-decision-tree-4.1.1")
   images+=("vcf-inheritance-matcher-3.1.0")
   images+=("vcf-report-6.0.2")
