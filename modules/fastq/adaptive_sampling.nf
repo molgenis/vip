761628581b32eb4eb939cfaf47607576013d211e
dennishendriksen <d.hendriksen@umcg.nl>
2024-06-18 10:28:25 +0200

    fastq workflow: create one qc report, performance improvements
    fix syntax error
    
    fix error
    
    fix stubbing error
    
    remove fastq nr/total
    
    fix
    
    fixes
    
    update docs: add fastp.options
    
    fix formatting
    
    remove unused utils
    
    minimap2_align_paired_end: add input path arity
    
    merge markdup with minimap2 process
    
    fastp fixes

--- modules/fastq/adaptive_sampling.nf
+++ modules/fastq/adaptive_sampling.nf
@@ -2,18 +2,18 @@ process filter_reads {
   label 'filter_reads'
 
   input:
-    tuple val(meta), path(fastq), path(adaptiveSamplingCsv)
+    tuple val(meta), path(fastqs, arity: '1..*'), path(adaptiveSamplingCsv)
 
   output:
   	tuple val(meta), path(fastqOut)
 
   shell:
-    fastqOut="${meta.project.id}_${meta.sample.family_id}_${meta.sample.individual_id}_filtered_${meta.sample.fastq.index}.fastq.gz"
+    fastqOut="${meta.project.id}_${meta.sample.family_id}_${meta.sample.individual_id}_filtered.fastq.gz"
 
     template 'adaptive_sampling_filter_reads.sh'
 
   stub:
-    fastqOut="${meta.project.id}_${meta.sample.family_id}_${meta.sample.individual_id}_filtered_${meta.sample.fastq.index}.fastq.gz"
+    fastqOut="${meta.project.id}_${meta.sample.family_id}_${meta.sample.individual_id}_filtered.fastq.gz"
 
     """
     touch "${fastqOut}"
--- modules/fastq/templates/minimap2_align_paired_end.sh
+++ modules/fastq/templates/minimap2_align_paired_end.sh
@@ -2,31 +2,6 @@
 set -euo pipefail
 
 align() {
-    local args_fastp=()
-    args_fastp+=("--thread" "!{task.cpus}")
-    if [[ "!{disable_quality_filtering}" == "true"  ]]; then
-        args_fastp+=("--disable_quality_filtering")
-    fi
-    if [[ "!{disable_length_filtering}" == "true"  ]]; then
-        args_fastp+=("--disable_length_filtering")
-    fi
-    if [[ "!{disable_adapter_trimming}" == "true"  ]]; then
-        args_fastp+=("--disable_adapter_trimming")
-    fi
-    if [[ "!{disable_trim_poly_g}" == "true"  ]]; then
-        args_fastp+=("--disable_trim_poly_g")
-    fi
-  if [[ -n "!{additional_params}" ]]; then
-    for param in !{additional_params}
-    do
-       args_fastp+=("${param}")
-    done
-  fi
-    args_fastp+=("--stdout")
-    args_fastp+=("--html" "!{reportFile}")
-    args_fastp+=("--in1" "!{fastqR1}")
-    args_fastp+=("--in2" "!{fastqR2}")
-
     local args=()
     args+=("-t" "!{task.cpus}")
     args+=("-a")
@@ -37,18 +12,19 @@ align() {
         args+=("-Y")
     fi
     args+=("!{referenceMmi}")
-    args+=("-")
+    args+=("!{fastqR1}")
+    args+=("!{fastqR2}")
 
-    ${CMD_FASTP} "${args_fastp[@]}" | \
-    ${CMD_MINIMAP2} "${args[@]}" | \
-    ${CMD_SAMTOOLS} fixmate --no-PG -u -m -@ "!{task.cpus}" - - | \
-    #position sort for markdup
-    ${CMD_SAMTOOLS} sort --no-PG -u -@ "!{task.cpus}" --reference "!{reference}" -o "!{cram}" --write-index -
-}
-
-publish_fastp() {
-  mkdir -p "!{outputPath}"
-  cp "!{reportFile}" "!{outputPath}/!{reportFile}"
+    if [[ "!{markDuplicates}" == "true" ]]; then
+      ${CMD_MINIMAP2} "${args[@]}" | \
+        ${CMD_SAMTOOLS} fixmate -u -m -@ "!{task.cpus}" --no-PG - - | \
+        ${CMD_SAMTOOLS} sort -u -@ "!{task.cpus}" --reference "!{reference}" --no-PG - | \
+        ${CMD_SAMTOOLS} markdup -@ "!{task.cpus}" --reference "!{reference}" --no-PG --write-index - "!{cram}"
+    else
+      ${CMD_MINIMAP2} "${args[@]}" |\
+        ${CMD_SAMTOOLS} fixmate -u -m -@ "!{task.cpus}" --no-PG - - | \
+        ${CMD_SAMTOOLS} sort -@ "!{task.cpus}" --reference "!{reference}" -o "!{cram}" --no-PG --write-index -
+    fi
 }
 
 stats() {
@@ -57,7 +33,6 @@ stats() {
 
 main() {
   align
-  publish_fastp
   stats
 }
 
