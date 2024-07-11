b70d32315b6f32fe5daae5c4d81933422e61c004
bartcharbon <b.charbon@umcg.nl>
2024-07-11 07:22:41 +0000

    Add giab testcase

---
+++ test/suites/giab/NA12878_nanopore_bam.sh
@@ -0,0 +1,34 @@
+#!/bin/bash
+set -euo pipefail
+
+# shellcheck disable=SC1091
+source "${TEST_UTILS_DIR}/utils.sh"
+
+base_url="https://ftp-trace.ncbi.nlm.nih.gov/ReferenceSamples/giab/data"
+
+declare -A files=()
+files["${base_url}/NA12878/Ultralong_OxfordNanopore/NA12878-minion-ul_GRCh38.bam"]="f4a7702cf82ea2586396e485567fa6f8"
+files["${base_url}/NA12878/Ultralong_OxfordNanopore/NA12878-minion-ul_GRCh38.bam.bai"]="0654a1ebef25da1753c830b007854a58"
+
+for i in "${!files[@]}"; do
+  download "${i}" "${files[$i]}" "${TEST_RESOURCES_DIR}/downloads"
+done
+
+args=()
+args+=("--workflow" "cram")
+args+=("--input" "${TEST_RESOURCES_DIR}/NA12878_nanopore_bam.tsv")
+args+=("--output" "${OUTPUT_DIR}")
+args+=("--resume")
+
+vip "${args[@]}" 1> /dev/null
+
+# compare expected to actual output and store result
+if [ "$(zcat "${OUTPUT_DIR}/vip.vcf.gz" | grep -vc "^#")" -gt 0 ]; then
+  result="0"
+else
+  result="1"
+fi
+echo -n "${result}" > "${OUTPUT_DIR}/.exitcode"
+
+# always exit with success error code
+exit 0
