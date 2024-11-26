8fc7b2dddad767dbec27c2cc8d840f62d1bb1a39
bartcharbon <b.charbon@umcg.nl>
2024-11-26 14:43:32 +0000

    Updated decision tree and inheritance modules

--- utils/apptainer/build.sh
+++ utils/apptainer/build.sh
@@ -96,8 +96,8 @@ main() {
   images+=("spectre-0.2.1-patched_v2")
   images+=("stranger-0.8.1_v2")
   images+=("straglr-1.4.4_vip_v3")
-  images+=("vcf-decision-tree-4.1.4")
-  images+=("vcf-inheritance-matcher-3.2.1")
+  images+=("vcf-decision-tree-5.1.0")
+  images+=("vcf-inheritance-matcher-3.3.0")
   images+=("vcf-report-7.0.3")
 
   for i in "${!images[@]}"; do
