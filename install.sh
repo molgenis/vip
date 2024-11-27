0dbe10acc94c0998c48708f86ee04ecc7a1b6a35
bartcharbon <b.charbon@umcg.nl>
2024-11-27 10:26:13 +0000

    inheritance match v3.3.2

--- install.sh
+++ install.sh
@@ -88,7 +88,7 @@ download_files() {
   urls+=("8f6e06847776448e004df8b863571109" "images/straglr-1.4.4_vip_v3.sif")
   urls+=("9c69ac645e04b91c8f480289c536429c" "images/stranger-0.8.1_v2.sif")
   urls+=("8575e48d7da8a7f0728d08dc204e42a5" "images/vcf-decision-tree-5.1.1.sif")
-  urls+=("cd149c9cc4266e4c4c8e596a52cf6cd8" "images/vcf-inheritance-matcher-3.3.1.sif")
+  urls+=("" "images/vcf-inheritance-matcher-3.3.2.sif")
   urls+=("87b2d9031b1b8351d2da14dd0095fbea" "images/vcf-report-7.0.3.sif")
   urls+=("7bffc236a7c65b2b2e2e5f7d64beaa87" "images/vep-111.0.sif")
   urls+=("82be3c18406e7c027ee4cec83a723d71" "nextflow-24.04.2-all")
