6af89d3d9b35677590ff357100efd4acd39ac375
Dennis Hendriksen <d.hendriksen@umcg.nl>
2024-12-02 14:10:13 +0100

    fix:bump vip-report-template v7.0.5 to v7.0.6, improve decision tree exit node labelling (#655)
    * feat:improve decision tree exit node labelling
    
    * fix:bump vip-report-template v7.0.5 to v7.0.6

--- install.sh
+++ install.sh
@@ -147,7 +147,7 @@ download_files() {
   # update utils/install.sh when updating inheritance.tsv
   urls+=("df31eb0fe9ebd9ae26c8d6f5f7ba6e57" "resources/inheritance_20240115.tsv")
   urls+=("7138e76a38d6f67935699d06082ecacf" "resources/vep/cache/homo_sapiens_refseq_vep_111_GRCh38.tar.gz")
-  urls+=("6949ed9cbbdfdc4c7744ea4a6756687d" "resources/vip-report-template-v7.0.5.html")
+  urls+=("d79bde5e130fb268c3b96d0beba2abd2" "resources/vip-report-template-v7.0.6.html")
   # when modifying urls array, please keep list in 'ls -l' order
   for ((i = 0; i < ${#urls[@]}; i += 2)); do
     download_file "${base_url}" "${urls[i+1]}" "${urls[i+0]}" "${output_dir}" "${validate}"
