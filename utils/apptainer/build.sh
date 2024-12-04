950840bfbd77919712fc536551403c68b5fa0ca3
bartcharbon <b.charbon@umcg.nl>
2024-12-04 11:20:37 +0000

    fix typo

--- utils/apptainer/build.sh
+++ utils/apptainer/build.sh
@@ -80,7 +80,7 @@ main() {
   images+=("build/openjdk-21")
   images+=("build/ubuntu-22.04")
   images+=("bcftools-1.21")
-  images+=("annotsv-3.4.3")
+  images+=("annotsv-3.4.4")
   images+=("capice-5.1.2")
   images+=("cutesv-2.1.1_v2")
   images+=("expansionhunter-5.0.0_v2")
