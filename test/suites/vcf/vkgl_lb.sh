4a672d979227d36b681ec78af08323e60c7049c4
dennishendriksen <d.hendriksen@umcg.nl>
2025-01-28 15:42:07 +0100

    docs:fix flappy vkgl_lb test case

--- test/suites/vcf/vkgl_lb.sh
+++ test/suites/vcf/vkgl_lb.sh
@@ -11,7 +11,8 @@ args+=("--resume")
 vip.sh "${args[@]}" 1> /dev/null
 
 # compare expected to actual output and store result
-if [ "$(zcat "${OUTPUT_DIR}/vip.vcf.gz" | grep -vc "^#")" -le 4528 ]; then
+# +50 because of non-deterministic behavior, see https://github.com/molgenis/vip/issues/604
+if [ "$(zcat "${OUTPUT_DIR}/vip.vcf.gz" | grep -vc "^#")" -le $((4528 + 50)) ]; then
   result="0"
 else
   result="1"
