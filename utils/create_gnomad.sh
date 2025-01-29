9fbd5a00baedd9a438da744614eb0db841b10206
dennishendriksen <d.hendriksen@umcg.nl>
2025-01-29 08:18:00 +0100

    fix:remove UMCG HPC specific config from create_gnomad.sh

--- utils/create_gnomad.sh
+++ utils/create_gnomad.sh
@@ -4,9 +4,9 @@ set -euo pipefail
 SCRIPT_DIR=$(dirname "$(realpath "$0")")
 
 APPTAINER_CACHEDIR="put_your_dir_here"
-CMD_BCFTOOLS="apptainer exec --no-mount home --bind /groups ${APPTAINER_CACHEDIR}/bcftools-1.20.sif bcftools"
-CMD_BGZIP="apptainer exec --no-mount home --bind /groups ${APPTAINER_CACHEDIR}/vep-111.0.sif bgzip"
-CMD_TABIX="apptainer exec --no-mount home --bind /groups ${APPTAINER_CACHEDIR}/vep-111.0.sif tabix"
+CMD_BCFTOOLS="apptainer exec --no-mount home ${APPTAINER_CACHEDIR}/bcftools-1.20.sif bcftools"
+CMD_BGZIP="apptainer exec --no-mount home ${APPTAINER_CACHEDIR}/vep-111.0.sif bgzip"
+CMD_TABIX="apptainer exec --no-mount home ${APPTAINER_CACHEDIR}/vep-111.0.sif tabix"
 
 THREADS=4
 
