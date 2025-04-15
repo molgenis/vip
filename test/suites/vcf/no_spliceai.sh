3f8df71f0876741d16993d7b9ef5dfb1eb5cd273
MatthieuBeukers <matthieu.beukers@gmx.com>
2025-04-15 07:45:31 +0000

    Fix num of variants in spliceai test

--- test/suites/vcf/no_spliceai.sh
+++ test/suites/vcf/no_spliceai.sh
@@ -13,7 +13,7 @@ args+=("--resume")
 runVip "${args}" "${TEST_RESOURCES_DIR}/no_spliceai.tsv"
 
 # compare expected to actual output and store result
-if [ "$(zcat "${OUTPUT_DIR}/vip.vcf.gz" | grep -vc "^#")" -eq 1 ]; then
+if [ "$(zcat "${OUTPUT_DIR}/vip.vcf.gz" | grep -vc "^#")" -eq 3 ]; then
   result="0"
 else
   result="1"
