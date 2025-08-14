c5fe88e159ecf32622fc5ce88af699529583352c
MatthieuBeukers <matthieu.beukers@gmx.com>
2025-08-14 09:23:12 +0000

    change vcf test threshold values

--- test/suites/vcf/vkgl_lb.sh
+++ test/suites/vcf/vkgl_lb.sh
@@ -14,7 +14,7 @@ runVip "${args}" "${TEST_RESOURCES_DIR}/vkgl_lb.tsv"
 
 # compare expected to actual output and store result
 # +50 because of non-deterministic behavior, see https://github.com/molgenis/vip/issues/604
-if [ "$(zcat "${OUTPUT_DIR}/vip.vcf.gz" | grep -vc "^#")" -le $((4777 + 50)) ]; then
+if [ "$(zcat "${OUTPUT_DIR}/vip.vcf.gz" | grep -vc "^#")" -le $((4981 + 50)) ]; then
   result="0"
 else
   result="1"
