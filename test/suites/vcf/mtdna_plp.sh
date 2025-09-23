629da59d5364093dfed4a9a9f295e56bc8ae4135
MatthieuBeukers <matthieu.beukers@gmx.com>
2025-09-23 07:02:47 +0000

    test: Add mtdna BLB and PLP unittests and necesasry files

---
+++ test/suites/vcf/mtdna_plp.sh
@@ -0,0 +1,24 @@
+#!/bin/bash
+set -euo pipefail
+
+# shellcheck disable=SC1091
+source "${TEST_UTILS_DIR}/utils.sh"
+
+args=()
+args+=("--workflow" "vcf")
+args+=("--config" "${TEST_RESOURCES_DIR}/mtdna_plp.cfg")
+args+=("--output" "${OUTPUT_DIR}")
+args+=("--resume")
+
+runVip "${args}" "${TEST_RESOURCES_DIR}/mtdna_plp.tsv"
+
+# compare expected to actual output and store result
+if [ "$(zcat "${OUTPUT_DIR}/vip.vcf.gz" | grep -vc "^#")" -ge 72 ]; then
+  result="0"
+else
+  result="1"
+fi
+echo -n "${result}" > "${OUTPUT_DIR}/.exitcode"
+
+# always exit with success error code
+exit 0
