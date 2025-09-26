9e3e8a1e0a96fba810d28a34e766fd7b4e53690a
MatthieuBeukers <matthieu.beukers@gmx.com>
2025-09-26 06:17:59 +0000

    change check to le 85 as 84 BLB varaints are misclassified and should not become more in tu future

--- test/suites/vcf/mtdna_blb.sh
+++ test/suites/vcf/mtdna_blb.sh
@@ -13,7 +13,7 @@ args+=("--resume")
 runVip "${args}" "${TEST_RESOURCES_DIR}/mtdna_blb.tsv"
 
 # compare expected to actual output and store result
-if [ "$(zcat "${OUTPUT_DIR}/vip.vcf.gz" | grep -vc "^#")" -ge 0 ]; then
+if [ "$(zcat "${OUTPUT_DIR}/vip.vcf.gz" | grep -vc "^#")" -le 85 ]; then
   result="0"
 else
   result="1"
