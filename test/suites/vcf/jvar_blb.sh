c335ebcf4802290b6a2d8e6a804db4fd3c9dcff8
MatthieuBeukers <matthieu.beukers@gmx.com>
2025-11-05 07:34:22 +0000

    test/Add japanese BLB and PLP variants tests

---
+++ test/suites/vcf/jvar_blb.sh
@@ -0,0 +1,24 @@
+#!/bin/bash
+set -euo pipefail
+
+# shellcheck disable=SC1091
+source "${TEST_UTILS_DIR}/utils.sh"
+
+args=()
+args+=("--workflow" "vcf")
+args+=("--config" "${TEST_RESOURCES_DIR}/jvar_blb.cfg")
+args+=("--output" "${OUTPUT_DIR}")
+args+=("--resume")
+
+runVip "${args}" "${TEST_RESOURCES_DIR}/jvar_blb.tsv"
+
+# compare expected to actual output and store result
+if [ "$(zcat "${OUTPUT_DIR}/vip.vcf.gz" | grep -vc "^#")" -ge 982 ]; then
+  result="0"
+else
+  result="1"
+fi
+echo -n "${result}" > "${OUTPUT_DIR}/.exitcode"
+
+# always exit with success error code
+exit 0
