585ba94894359309e0d05e57c209bf23cc20c379
MatthieuBeukers <matthieu.beukers@gmx.com>
2025-12-18 11:27:44 +0000

    Update nextflow to 25.10.2

--- vip.sh
+++ vip.sh
@@ -137,7 +137,7 @@ execute_workflow() {
   fi
   local envStrict="true"
 
-  local -r nextflow_version="24.10.6"
+  local -r nextflow_version="25.10.2"
 
   local args=()
   args+=("-C" "${configs}")
