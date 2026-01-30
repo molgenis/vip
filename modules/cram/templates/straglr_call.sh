8a0b49f54db5a362168ba482a791022ff6759363
bart.charbon <b.charbon@umcg.nl>
2026-01-30 11:43:42 +0100

    Move to bcgsc straglr version

--- modules/cram/templates/straglr_call.sh
+++ modules/cram/templates/straglr_call.sh
@@ -5,8 +5,6 @@ call_short_tandem_repeats () {
     local args=()
     args+=("--loci" "!{paramLoci}")
     args+=("--sample" "!{sampleId}")
-    args+=("--vcf" "straglr.vcf")
-    args+=("--tsv" "!{tsvOut}")
     if [ -n "!{sampleSex}" ]; then
         args+=("--sex" "!{sampleSex}")
     fi
@@ -14,22 +12,40 @@ call_short_tandem_repeats () {
     args+=("--min_cluster_size" "!{paramMinClusterSize}")
     args+=("!{cram}")
     args+=("!{paramReference}")
+    args+=("straglr")
 
     ${CMD_STRAGLR} "${args[@]}"
+
+    mv straglr.tsv "!{tsvOut}"
+    rm straglr.vcf
 }
 
 index () {
-  # workaround for https://github.com/molgenis/vip/issues/471
-  ${CMD_BCFTOOLS} reheader --fai "!{paramReferenceFai}" --temp-prefix . --threads "!{task.cpus}" "straglr.vcf" | ${CMD_BCFTOOLS} sort --temp-dir . --max-mem "!{task.memory.toGiga() - 1}G" --output-type z --output "!{vcfOut}"
-
   ${CMD_BCFTOOLS} index --csi --output "!{vcfOutIndex}" --threads "!{task.cpus}" "!{vcfOut}"
   ${CMD_BCFTOOLS} index --stats "!{vcfOut}" > "!{vcfOutStats}"
+}
 
-  rm straglr.vcf
+tsv2vcf() {
+  local args=()
+  args+=("-Djava.io.tmpdir=\"${TMPDIR}\"")
+  args+=("-XX:ParallelGCThreads=2")
+  args+=("-Xmx!{task.memory.toMega() - 512}m")
+  args+=("-jar" "/opt/straglr-tsv2vcf/lib/straglrTsv2Vcf.jar")
+  args+=("--input" ""!{tsvOut}")
+  args+=("--loci" "!{paramLoci}")
+  args+=("--reference" "!{paramReference}")
+  if [ "!{sampleSex}" == "male" ]; then
+    args+=("--haploid_contigs" "!{haploidContigsMale}")
+  fi
+  args+=("--sample" "!{sampleId}")
+  args+=("--output" "!{vcfOut}")
+
+  ${CMD_STRAGLR_TSV2VCF} java "${args[@]}"
 }
 
 main() {
     call_short_tandem_repeats
+    tsv2vcf
     index
 }
 
