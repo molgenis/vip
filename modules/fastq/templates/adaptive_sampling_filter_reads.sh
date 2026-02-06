d7e117667f23d3d83d3edfbe46a58fbe0e179491
bartcharbon <b.charbon@umcg.nl>
2026-02-06 09:21:49 +0000

    Remove debug logging

--- modules/fastq/templates/adaptive_sampling_filter_reads.sh
+++ modules/fastq/templates/adaptive_sampling_filter_reads.sh
@@ -2,29 +2,23 @@
 set -euo pipefail
 
 get_stop_receiving_read_ids() {
-  echo "1"
   # For files produced by MinKnow < 26:
   { grep stop_receiving "!{adaptiveSamplingCsv}" || true; } | cut -d , -f5 >> accepted_read_ids.txt
-  echo "2"
   # For files produced by MinKnow > 26:
   { grep sequence "!{adaptiveSamplingCsv}" || true; } | cut -d , -f5 >> accepted_read_ids.txt
-  echo "3"
 }
 
 concat() {
-  echo "5"
   # concatenate both compressed and uncompressed files
   zcat --force !{fastqs}
 }
 
 filter_reads() {
-  echo "4"
   # prevent writing merged fastq to disk
   concat | ${CMD_SEQTK} subseq - accepted_read_ids.txt | gzip > "!{fastqOut}"
 }
 
 main() {
-  echo "0"
   get_stop_receiving_read_ids
   filter_reads
 }
