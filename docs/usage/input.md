1caa40e468cb286b81bde55201a77df1c420854e
bartcharbon <b.charbon@umcg.nl>
2026-02-10 10:16:08 +0000

    Stricter adaptive sampling file matching

--- docs/usage/input.md
+++ docs/usage/input.md
@@ -54,7 +54,7 @@ specific columns.
 | ``sequencing_platform`` | ``enum``      |                 | ``nanopore`` | allowed values: [``illumina``,``nanopore``,``pacbio_hifi``], value must be the same for all project samples                                                     |
 
 <sup>2</sup> Either the `fastq` or the ``fastq_r1`` and ``fastq_r2`` are required.
-<sup>3</sup> `stop_receiving` when [MinKnow](https://nanoporetech.com/document/experiment-companion-minknow) software version 25 or lower was used, `sequence` for MinKnow 26 and above.
+<sup>3</sup> `stop_receiving` when [Nanopore output](https://software-docs.nanoporetech.com/output-specifications) software version 25 or lower was used, `sequence` for version 26 and above.
 
 ## Columns: CRAM
 
--- modules/fastq/templates/adaptive_sampling_filter_reads.sh
+++ modules/fastq/templates/adaptive_sampling_filter_reads.sh
@@ -2,10 +2,40 @@
 set -euo pipefail
 
 get_stop_receiving_read_ids() {
-  # For files produced by MinKnow < 26:
-  { grep stop_receiving "!{adaptiveSamplingCsv}" || true; } | cut -d , -f 5 >> accepted_read_ids.txt
-  # For files produced by MinKnow > 26:
-  { grep sequence "!{adaptiveSamplingCsv}" || true; } | cut -d , -f 1 >> accepted_read_ids.txt
+header=$(head -n1 "!{adaptiveSamplingCsv}")
+
+# For files produced by Nanopore output specification < 26:
+if [[ "$header" == "batch_time,read_number,channel,num_samples,read_id,sequence_length,decision" ]]; then
+    readIdCol=5
+    statusCol=7
+    version="v1"
+# For files produced by Nanopore output specification >= 26:
+# https://software-docs.nanoporetech.com/output-specifications/26.01/
+# batch_time,...,read_id,sequence_length,decision
+elif [[ "$header" == "read_id,action,action_response" ]]; then
+    # read_id,action,action_response
+    readIdCol=1
+    statusCol=2
+    version="v2"
+else
+    echo "ERROR: Unrecognized adaptive sampling file format: $header" >&2
+    exit 1
+fi
+
+case "$version" in
+  "v1")
+    awk -F',' -v read_id="$readIdCol" -v status="$statusCol" '
+      NR==1 {next}
+      $status == "stop_receiving" {print $read_id}
+    ' "!{adaptiveSamplingCsv}" > "accepted_read_ids.txt"
+    ;;
+  "v2")
+    awk -F',' -v read_id="$readIdCol" -v status="$statusCol" '
+      NR==1 {next}          # skip header
+      $status == "sequence" {print $read_id}
+    ' "!{adaptiveSamplingCsv}" > "accepted_read_ids.txt"
+    ;;
+esac
 }
 
 concat() {
