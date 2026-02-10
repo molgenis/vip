e59f348c96d243eafbafb59eb044247cdd3f417d
bartcharbon <b.charbon@umcg.nl>
2026-02-10 11:59:43 +0000

    Check for response = SUCCESS in adaptive sampling

--- modules/fastq/templates/adaptive_sampling_filter_reads.sh
+++ modules/fastq/templates/adaptive_sampling_filter_reads.sh
@@ -4,18 +4,19 @@ set -euo pipefail
 get_stop_receiving_read_ids() {
 header=$(head -n1 "!{adaptiveSamplingCsv}")
 
-# For files produced by Nanopore output specification < 26:
+# https://software-docs.nanoporetech.com/output-specifications/latest/minknow/adaptive_sampling/adaptive_sampling_decisions/
+# For adaptive sampling file specification < 0.1:
 if [[ "$header" == "batch_time,read_number,channel,num_samples,read_id,sequence_length,decision" ]]; then
     readIdCol=5
     statusCol=7
     version="v1"
-# For files produced by Nanopore output specification >= 26:
-# https://software-docs.nanoporetech.com/output-specifications/26.01/
+# For adaptive sampling file specification >= 0.1:
 # batch_time,...,read_id,sequence_length,decision
 elif [[ "$header" == "read_id,action,action_response" ]]; then
     # read_id,action,action_response
     readIdCol=1
     statusCol=2
+    responseCol=3
     version="v2"
 else
     echo "ERROR: Unrecognized adaptive sampling file format: $header" >&2
@@ -30,9 +31,9 @@ case "$version" in
     ' "!{adaptiveSamplingCsv}" > "accepted_read_ids.txt"
     ;;
   "v2")
-    awk -F',' -v read_id="$readIdCol" -v status="$statusCol" '
-      NR==1 {next}          # skip header
-      $status == "sequence" {print $read_id}
+    awk -F',' -v read_id="$readIdCol" -v status="$statusCol" -v response="$responseCol" '
+      NR==1 {next}
+      $status == "sequence" && $response == "SUCCESS" {print $read_id}
     ' "!{adaptiveSamplingCsv}" > "accepted_read_ids.txt"
     ;;
 esac
