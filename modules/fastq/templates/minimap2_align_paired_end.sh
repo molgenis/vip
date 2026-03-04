3a300329706744809f2e0fcc6d30f6bf5f4cdc5b
bartcharbon <b.charbon@umcg.nl>
2026-03-04 10:46:26 +0000

    fix index for markdup

--- modules/fastq/templates/minimap2_align_paired_end.sh
+++ modules/fastq/templates/minimap2_align_paired_end.sh
@@ -23,7 +23,7 @@ align() {
         ${CMD_SAMTOOLS} markdup -@ "!{task.cpus}" --reference "!{reference}" --no-PG - - | \
         ${CMD_SAMTOOLS} view  --cram --output "!{cram}" --target-file "!{bedFile}" --reference "!{reference}" --write-index --no-PG --threads "!{task.cpus}" -
       else
-        ${CMD_SAMTOOLS} markdup -@ "!{task.cpus}" --reference "!{reference}" --no-PG - "!{cram}"
+        ${CMD_SAMTOOLS} markdup -@ "!{task.cpus}" --reference "!{reference}" --no-PG  --write-index - "!{cram}"
       fi
     else
       ${CMD_MINIMAP2} "${args[@]}" |\
