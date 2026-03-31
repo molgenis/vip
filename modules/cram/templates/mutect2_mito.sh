08b2016c9c3a13c47a5c979ce3ce54101aa732c1
MatthieuBeukers <matthieu.beukers@gmx.com>
2026-03-31 05:58:25 +0000

    Add gatk command back to config and remove own cram 3.1 workaround

--- modules/cram/templates/mutect2_mito.sh
+++ modules/cram/templates/mutect2_mito.sh
@@ -1,15 +1,3 @@
-filter_cram () {
-    local filter_args=()
-    filter_args+=("-T" "!{reference}")
-    filter_args+=("-b")
-    filter_args+=("-o" "!{cram}.chrm.bam")
-    filter_args+=("-e" "rname == \"!{chrmName}\"")
-    filter_args+=("!{cram}")
-
-    ${CMD_SAMTOOLS} view "${filter_args[@]}"
-    ${CMD_SAMTOOLS} index "!{cram}.chrm.bam"
-}
-
 mutect2 () {
     local mutect_args=()
     mutect_args+=("-Djava.io.tmpdir=${TMPDIR}")
@@ -20,7 +8,7 @@ mutect2 () {
     mutect_args+=("-R" "!{reference}")
     mutect_args+=("-L" "!{chrmName}")
     mutect_args+=("--mitochondria-mode")
-    mutect_args+=("-I" "!{cram}.chrm.bam")
+    mutect_args+=("-I" "!{cram}")
     mutect_args+=("-O" "!{vcfOut}")
     mutect_args+=("--max-reads-per-alignment-start" "0")
 
@@ -32,16 +20,9 @@ index () {
     ${CMD_BCFTOOLS} stats "!{vcfOut}" > "!{vcfOutStats}"
 }
 
-cleanup () {
-    rm "!{cram}.chrm.bam"
-    rm "!{cram}.chrm.bam.bai"
-}
-
 main () {
-    filter_cram
     mutect2
     index
-    cleanup
 }
 
 main "$@"
