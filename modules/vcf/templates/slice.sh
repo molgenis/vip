2055678078e0baeb285fa787230d72d99c01b134
Dennis Hendriksen <d.hendriksen@umcg.nl>
2026-04-03 12:50:42 +0200

    feat:strip cram (#814)
    * fix:cram slice .bed end is exclusive
    
    * feat:strip cram

--- modules/vcf/templates/slice.sh
+++ modules/vcf/templates/slice.sh
@@ -10,33 +10,53 @@ create_bed () {
     args+=("!{vcf}")
     
     # -1 because positions in .bed are 0-based and 1-based in .vcf
-    ${CMD_BCFTOOLS} "${args[@]}" | awk -v FS='\t' -v OFS='\t' '{print $1 "\t" $2-1 "\t" $2-1 "\t"}' > "!{vcf.simpleName}.bed"
+    ${CMD_BCFTOOLS} "${args[@]}" | awk -v FS='\t' -v OFS='\t' '{print $1 "\t" $2-1 "\t" $2 "\t"}' > "!{vcf.simpleName}.bed"
 }
 
-slice () {
-    local args=()
-    args+=("view")
-    args+=("--cram")
-    args+=("--output" "!{cramOut}")
-    args+=("--target-file" "!{vcf.simpleName}.bed")
-    args+=("--reference" "!{refSeqPath}")
-    args+=("--output-fmt-option" "level=9")
-    args+=("--output-fmt-option" "archive")
-    # not supported by igv.js v2.13.3
-    args+=("--output-fmt-option" "use_lzma=0")
-    # not supported by igv.js v2.13.3
-    args+=("--output-fmt-option" "use_bzip2=0")
-    args+=("--write-index")
-    args+=("--no-PG")
-    args+=("--threads" "!{task.cpus}")
-    args+=("!{cram}")
+slice_and_strip () {
+    local slice_args=()
+    slice_args+=("view")
+    slice_args+=("--with-header")
+    slice_args+=("--threads" "!{task.cpus}")
+    slice_args+=("--target-file" "!{vcf.simpleName}.bed")
+    slice_args+=("--reference" "!{refSeqPath}")
+    slice_args+=("--no-PG")
+    slice_args+=("!{cram}")
 
-    ${CMD_SAMTOOLS} "${args[@]}"
+    local strip_args=()
+    strip_args+=("view")
+    strip_args+=("--cram")
+    strip_args+=("--output" "!{cramOut}")
+    strip_args+=("--reference" "!{refSeqPath}")
+    strip_args+=("--output-fmt-option" "level=9")
+    strip_args+=("--output-fmt-option" "archive")
+    # not supported by igv.js v2.13.3
+    strip_args+=("--output-fmt-option" "use_lzma=0")
+    # not supported by igv.js v2.13.3
+    strip_args+=("--output-fmt-option" "use_bzip2=0")
+    strip_args+=("--write-index")
+    strip_args+=("--no-PG")
+    strip_args+=("--threads" "!{task.cpus}")
+
+    ${CMD_SAMTOOLS} "${slice_args[@]}" | \
+    awk -v FS='\t' -v OFS='\t' '
+      /^@/ {print; next}
+      {
+          # drop QUAL
+          $11="*"
+
+          # print core SAM fields
+          printf "%s\t%s\t%s\t%s\t%s\t%s\t%s\t%s\t%s\t%s\t%s",
+                 $1,$2,$3,$4,$5,$6,$7,$8,$9,$10,$11
+
+          print ""
+      }' | \
+    ${CMD_SAMTOOLS} "${strip_args[@]}"
 }
 
 main() {
     create_bed
-    slice
+    slice_and_strip
 }
 
 main "$@"
